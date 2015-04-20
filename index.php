25539c059580bb8cd47a0927de0ee480ed74fe43
zak000 <zak.essadeq@gmail.Com>
2015-04-20 15:09:11 +0200

    upload picture

--- index.php
+++ index.php
@@ -106,22 +106,22 @@
 					}
 				
        
-                echo "yes";
+                echo "---------------------";
                 
     // Upload to a user's profile. The photo will be in the
     // first album in the profile. You can also upload to
     // a specific album by using /ALBUM_ID as the path     
-    $response = (new FacebookRequest(
-      $session, 'POST', '/me/photos', array(
-        'url' => new CURLFile('./images/kitesurf_Optim.jpg', 'image/jpg'),
-        'message' => 'User provided message'
-      )
-    ))->execute()->getGraphObject();
+    //$response = (new FacebookRequest(
+      //$session, 'POST', '/me/photos', array(
+        //'url' => new CURLFile('./images/kitesurf_Optim.jpg', 'image/jpg'),
+        //'message' => 'User provided message'
+      //)
+    //))->execute()->getGraphObject();
 
     //// If you're not using PHP 5.5 or later, change the file reference to:
     // 'source' => '@/path/to/file.name'
 
-    echo "Posted with id: " . $response->getProperty('id');
+    //echo "Posted with id: " . $response->getProperty('id');
 
 
 
@@ -138,9 +138,27 @@
                 } else 
                 
                 {
-                $params = array(
-                  scope => 'read_stream,publish_stream,publish_actions,offline_access',
-                  redirect_uri => 'Redirect_URL'
+                    
+                        // Upload to a user's profile. The photo will be in the
+                        // first album in the profile. You can also upload to
+                        // a specific album by using /ALBUM_ID as the path     
+                        $response = (new FacebookRequest(
+                          $session, 'POST', '/me/photos', array(
+                            'url' => new CURLFile('./images/kitesurf_Optim.jpg', 'image/jpg'),
+                            'message' => 'User provided message'
+                          )
+                        ))->execute()->getGraphObject();
+
+                        //// If you're not using PHP 5.5 or later, change the file reference to:
+                        // 'source' => '@/path/to/file.name'
+
+                        echo "Posted with id: " . $response->getProperty('id');
+                    
+                    
+                    
+                        $params = array(
+                        scope => 'read_stream,publish_stream,publish_actions,offline_access',
+                        redirect_uri => 'Redirect_URL'
                 );
                   $loginUrl = $facebook->getLoginUrl($params);
                 }
