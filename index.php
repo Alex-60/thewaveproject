eadce806917739b5d13709744b5d2f56ad349050
zak000 <zak.essadeq@gmail.Com>
2015-04-21 10:17:20 +0200

    test the upload

--- index.php
+++ index.php
@@ -12,49 +12,35 @@
 	use Facebook\GraphUser;
     
   
-    const APPID = "767304380051847";
-    const APPSECRET ="7f0e4cac931818f7f7dc86d722dd5e0e";
+    const APPID = "1568780100068670";
+    const APPSECRET ="631a57c4f691dfe29a047a4097125480";
 
     FacebookSession::setDefaultApplication(APPID, APPSECRET);
 
-    $helper = new FacebookRedirectLoginHelper('https://thewave.herokuapp.com/');
+    $helper = new FacebookRedirectLoginHelper('https://projetrsesgi.herokuapp.com');
 	
 	//SI les variables de sessions existent et que $_SESSION['fb_token'] existe
 	// alors je veux créer mon utilisateur à partir de cette session
 	if( isset($_SESSION) && isset($_SESSION['fb_token']) )
 	{
-		$session = new FacebookSession($_SESSION['fb_token']); 
-        
+		$session = new FacebookSession($_SESSION['fb_token']);
 	}
 	//Sinon j'affiche le lien de connection
 	else
 	{
 		$session = $helper->getSessionFromRedirect();
 	}
-
+	
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    
-    <title>The Wave Project</title>
-    
-    <link rel="stylesheet" media="screen" type="text/css" title="simple" href="./bootstrap/css/wave.css" />
-    
-    <link href="stylesheet" media="screen" type="text/css" title="simple" href="./bootstrap/css/wave.css">
-    
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    
-    <link href="stylesheet" media="screen" type="text/css" title="simple" href="bootstrap/css/bootstrap.css">
-    
-    
-    
+    <title>Projet ESGI RS</title>
 </head>
 <body>
-    <script>
+   <script>
 		  window.fbAsyncInit = function() {
 		    FB.init({
 		      appId      : '<?php echo APPID; ?>',
@@ -69,61 +55,32 @@
 		     js.src = "//connect.facebook.net/fr_FR/sdk.js";
 		     fjs.parentNode.insertBefore(js, fjs);
 		   }(document, 'script', 'facebook-jssdk'));
-   </script>
-		
-	<div id="presentation">
-		<h1>The Wave Concours</h1>
+		</script>
    
-           <div>
-                  class="fb-like"
-                  data-share="true"
-                  data-width="450"
-                  data-show-faces="true">
-            </div>
-    
-    
+<h1>Mon Application Facebook</h1>
+   
+   <div
+      class="fb-like"
+      data-share="true"
+      data-width="450"
+      data-show-faces="true">
+    </div>
 
     <br>
        <?php
-			if($session)				
+			if($session)
 			{
-				try
-					{
-                    //show the user's informations
-                    
-						$_SESSION['fb_token'] = (string) $session->getAccessToken();
-						$request_user = new FacebookRequest( $session,"GET","/me");
-						$request_user_executed = $request_user->execute();
-						$user = $request_user_executed->getGraphObject(GraphUser::className());
-						var_dump($user);
-						echo "Bonjour ".$user->getName();
-                    
-                    //upload a user's picture
-                    
-             //$params = array('read_stream,publish_stream,publish_actions,offline_access');
-                    
-                    //$response = (new FacebookRequest(
-                         // $session, 'POST', '/me/photos', array(
-                            //'source' => new CURLFile('./images/kitesurf_Optim.jpg', 'image/jpg'),
-                            //'message' => 'User provided message'
-                          //)
-                        //))->execute()->getGraphObject($params);
-
-					}
-				catch (Exception $e)
-					{
-						$_SESSION = null;
-						session_destroy();
-						header('Location : index.php');
-					}
-				
-                echo "---------------------";
-                
-            }
-                    
+				$_SESSION['fb_token'] = (string) $session->getAccessToken();
+				$request_user = new FacebookRequest( $session,"GET","/me");
+				$request_user_executed = $request_user->execute();
+				$user = $request_user_executed->getGraphObject(GraphUser::className());
+				echo "Bonjour ".$user->getName();
+			}else{
+				$loginUrl = $helper->getLoginUrl();
+				echo "<a href='".$loginUrl."'>Se connecter</a>";
+			}
 		?>
+        
     
-    </div>
 </body>
 </html>
-
