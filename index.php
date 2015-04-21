df4c5b7b9beaaaa2aa9d8350093a4fa91db138e2
zak000 <zak.essadeq@gmail.Com>
2015-04-21 23:13:10 +0200

    poste pic

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     
         session_start();
 	
-        require "facebook-php-sdk-v4-4.0-dev/autoload.php";
+        require_once './facebook-php-sdk-v4-4.0-dev/autoload.php';
 
     use Facebook\FacebookSession;
 	use Facebook\FacebookRedirectLoginHelper;
