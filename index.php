6ad9c1b98218acb0f6487caf03acebfff4b20782
zak000 <zak.essadeq@gmail.Com>
2015-04-21 14:12:27 +0200

    load pictures

--- index.php
+++ index.php
@@ -92,16 +92,15 @@
                 
                 $response = (new FacebookRequest(
                   $session, 'POST', '/me/photos', array(
-                    //'url' => "./images/Kite_Surf.jpg",
+                    'url' => "./images/Kite_Surf.jpg",
                       
-                      'source' => ('./images/Kite_Surf.jpg', 'image/png'),
-                      //'source' => file_get_contents('./images/Kite_Surf.jpg'),
+                      //'source' => ('./images/Kite_Surf.jpg', 'image/png'),
+                      'source' => file_get_contents('./images/Kite_Surf.jpg'),
                     'message' => 'User provided message',
                   )
                 ))->execute()->getGraphObject();
                 
-                echo $response;
-                
+              
 			}
             else
             {
