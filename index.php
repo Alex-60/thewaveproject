c9ea02cd36a76e8dc5bd2b846235c8ec2ac13394
zak000 <zak.essadeq@gmail.Com>
2015-04-25 23:41:57 +0200

    get user picture

--- index.php
+++ index.php
@@ -208,7 +208,7 @@
 
                     $result = $graphObject->asArray();
                     
-                    var_dump($result[0]);
+                    var_dump($result[]);
                     
                     die();
                     
