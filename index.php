b01ddd4e3785aab9f75701787b65eab604b31a0a
zak000 <zak.essadeq@gmail.Com>
2015-04-26 01:29:12 +0200

    get user in array

--- index.php
+++ index.php
@@ -210,7 +210,7 @@
               //var_dump($result);
                     //die();
                 
-                $friends = $result['data'][0]['name'];
+                $friends = $result['data'][0][0];
                 
                     var_dump ($friends);
                     die();
