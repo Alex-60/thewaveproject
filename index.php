f866e5e3164efb17a4c4e450ccdef2383d21f3b8
zak000 <zak.essadeq@gmail.Com>
2015-04-25 18:29:37 +0200

    get user picture

--- index.php
+++ index.php
@@ -175,18 +175,26 @@
                 
            
                   
-                  $friends = $facebook->api('/me/friends');
+                <?php
+                        $user = $facebook->getUser();
 
-                    echo '<ul>';
-                    foreach ($friends["data"] as $value) {
-                        echo '<li>';
-                        echo '<div class="pic">';
-                        echo '<img src="https://graph.facebook.com/' . $value["id"] . '/picture"/>';
-                        echo '</div>';
-                        echo '<div class="picName">'.$value["name"].'</div>'; 
-                        echo '</li>';
-                    }
-                    echo '</ul>';
+
+                        if ($user) {
+                            $user_profile = $facebook->api('/me');
+                            $friends = $facebook->api('/me/friends');
+
+                            echo '<ul>';
+                            foreach ($friends["data"] as $value) {
+                                echo '<li>';
+                                echo '<div class="pic">';
+                                echo '<img src="https://graph.facebook.com/' . $value["id"] . '/picture"/>';
+                                echo '</div>';
+                                echo '<div class="picName">'.$value["name"].'</div>'; 
+                                echo '</li>';
+                            }
+                            echo '</ul>';
+                        }
+                    ?>
                 echo "--------------------------";
                 
                 
