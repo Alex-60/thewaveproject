e3ad9bb887027b99fed4cb82f62e339155dd5d68
zak000 <zak.essadeq@gmail.Com>
2015-04-25 23:14:43 +0200

    get user picture

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     
         session_start();
 	
-        //require_once 'facebook-php-sdk-v4-4.0-dev/autoload.php';
+    require_once 'facebook-php-sdk-v4-4.0-dev/autoload.php';
 
     use Facebook\FacebookSession;
 	use Facebook\FacebookRedirectLoginHelper;
@@ -213,7 +213,7 @@
                 
                 //var_dump($graphObject);
                 
-                $friends= $facebook->api('/me/friends?token='.$session['access_token']);
+                $friends= new FacebookRequest('/me/friends?token='.$session['access_token']);
                 var_dump($friends);
                     
                     
