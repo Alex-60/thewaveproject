193b0040dabbd096f7c46f6287bf95378564183f
zak000 <zak.essadeq@gmail.Com>
2015-04-26 13:21:02 +0200

    parse tableau

--- index.php
+++ index.php
@@ -228,6 +228,23 @@
                     echo "<p><img src='$image' /></p>";
                 
                 
+                echo "<p>----</p>"; 
+                $a = array();
+                $a[0][0] = "a";
+                $a[0][1] = "b";
+                $a[1][0] = "y";
+                $a[1][1] = "z";
+
+                foreach ($a as $v1) {
+                    foreach ($v1 as $v2) {
+                        echo "$v2\n";
+                    }
+                }
+                
+                
+                
+                
+                
                /* foreach ($friends as $key => $value) 
                 {
                     $friends[$key] = (array)$value;
