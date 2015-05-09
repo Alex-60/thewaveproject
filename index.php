b2e3ec2d4b7fb91a584cfda1ee51e4a7d9c4c1f9
zak000 <zak.essadeq@gmail.Com>
2015-05-09 23:06:55 +0200

    css tasks

--- index.php
+++ index.php
@@ -194,7 +194,7 @@
       
                     <?
                 
-                        $request = new FacebookRequest($session,'GET','/978420528843358?fields=likes');
+                        $request = new FacebookRequest($session,'GET','/1385753921748799?fields=likes');
                         $response = $request->execute();
                         $graphObject = $response->getGraphObject(GraphUser::className());
 
