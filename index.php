f17132f3ec56167248aabbc24034e7c490ee75d7
zak000 <zak.essadeq@gmail.Com>
2015-05-10 22:09:27 +0200

    css tasks

--- index.php
+++ index.php
@@ -228,7 +228,7 @@
 			}
             else
             {
-				$loginUrl = $helper->getLoginUrl(['publish_actions','users_likes','user_photos','user_posts','read_stream','user_friends','manage_pages','user_likes']);
+				$loginUrl = $helper->getLoginUrl(['publish_actions','user_likes','user_photos','user_posts','read_stream','user_friends','manage_pages','user_likes']);
 				echo "<a href='".$loginUrl."'>Se connecter</a>";
                 
                 
