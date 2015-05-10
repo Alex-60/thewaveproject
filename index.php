d48a236746dcbb611212549d4f490afabd0604f2
zak000 <zak.essadeq@gmail.Com>
2015-05-10 21:51:32 +0200

    css tasks

--- index.php
+++ index.php
@@ -200,7 +200,7 @@
 
                         $result = $graphObject->asArray();
                 
-                       var_dump($result);
+                       //var_dump($result);
                 
                        //var $a = SELECT user_id FROM like WHERE object_id="1385753921748799";
                 
@@ -210,6 +210,24 @@
                        
                 
                     ?>
+                    
+                    
+                    <div id="fb-root"></div>
+                    <script>(function(d, s, id) {
+                      var js, fjs = d.getElementsByTagName(s)[0];
+                      if (d.getElementById(id)) return;
+                      js = d.createElement(s); js.id = id;
+                      js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v2.3&appId=767304380051847";
+                      fjs.parentNode.insertBefore(js, fjs);
+                    }(document, 'script', 'facebook-jssdk'));</script>
+                    
+                    
+                    
+                    
+                    <div class="fb-like" data-href="https://developers.facebook.com/docs/plugins/" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
+                    
+                    
+                    
                 
                 </div>
         
