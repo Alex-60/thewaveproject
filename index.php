e8534411fed853450f30e9db33ac647f63ff3c66
zak000 <zak.essadeq@gmail.Com>
2015-05-11 22:56:21 +0200

    get fans

--- index.php
+++ index.php
@@ -141,7 +141,7 @@
                     
                     
                     
-<div class="fb-page" data-href="https://www.facebook.com/pages/The-Wave/1385753921748799" data-width="500" data-hide-cover="true" data-show-posts="true">
+<div class="fb-page" data-href="https://www.facebook.com/pages/The-Wave/1385753921748799" data-show-posts="true">
     
    
                     </div>
