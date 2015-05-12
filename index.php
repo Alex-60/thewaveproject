67b1fc4c1d19b77e92cf5e2092b48838370a919c
zak000 <zak.essadeq@gmail.Com>
2015-05-12 18:14:12 +0200

    bug fixed

--- index.php
+++ index.php
@@ -156,9 +156,9 @@
                          
                                // echo "<p>$value->id</p>";
 
-                               $image='https://graph.facebook.com/'.$value->id.'/picture?width=40';
+                         echo "<p>$value->picture</p>";
 
-                               echo "<p><img src='$image' /></p>";
+                               
                     
                              
                            
@@ -192,14 +192,14 @@
                                 echo "<p>$value->name</p>";
                          
                          
-                                echo "<p>$value->picture</p>";
+                               
                          
                          
                                // echo "<p>$value->id</p>";
 
-                               //$image='https://graph.facebook.com/'.$value->id.'/picture?width=40';
+                               $image='https://graph.facebook.com/'.$value->id.'/picture?width=40';
 
-                               //echo "<p><img src='$image' /></p>";
+                               echo "<p><img src='$image' /></p>";
                     
                                     ?>
                                     
