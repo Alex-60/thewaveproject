b3844e0eef11e3f6e6fdcfb6757aec112ebf1464
zak000 <zak.essadeq@gmail.Com>
2015-05-15 12:17:30 +0200

    photo change

--- index.php
+++ index.php
@@ -158,7 +158,7 @@
                     <div id="affiche_pic">
                     
                     <?
-                          echo "<p><img src='$value->picture' /></p>";
+                          echo "<p><img src='$value->full_picture' /></p>";
                    
                    
                    
