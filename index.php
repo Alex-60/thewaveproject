cff854d34303222785b39723c45ea12a8bf04b95
zak000 <zak.essadeq@gmail.Com>
2015-05-15 12:03:14 +0200

    photo change

--- index.php
+++ index.php
@@ -158,7 +158,7 @@
                     <div id="affiche_pic">
                     
                     <?
-                          echo "<p><img width="90" height="53" src='$value->picture'/></p>";
+                          echo "<p><img src='$value->picture.type(large)' /></p>";
                         
                         ?>                    
                     </div>
