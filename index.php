bb88e792bb5f4356068a01d7240abf6993acdb6f
zak000 <zak.essadeq@gmail.Com>
2015-05-23 22:39:03 +0200

    pictures resize

--- index.php
+++ index.php
@@ -172,7 +172,7 @@
                     {
 
                     ?>
-                        <div id ="border_posts" class="col-md-6">
+                        <div id ="border_posts" class="col-md-6" style="text-aligne:center;">
               
    
                             <?
