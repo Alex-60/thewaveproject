ad504e3c4f63afebbe91e74b2fb4c35a32e09672
zak000 <zak.essadeq@gmail.Com>
2015-06-17 16:33:44 +0200

    database issue

--- index.php
+++ index.php
@@ -307,6 +307,8 @@
                 {
                     $dbconn3 = pg_connect("host=ec2-54-83-25-238.compute-1.amazonaws.com port=5432 dbname=dfhf24ft89btrp user=iclwqstdcanbnn password=VdN3cktdfKZZzPnasW4IxrghX6");
                     echo "doug";
+                    echo $dbconn3;
+                    
                 }
                 catch (Exception $e)
                 {
