31cc2921cfcad943085bbf3820d2c185a5dcd3c8
zak000 <zak.essadeq@gmail.Com>
2015-06-17 23:40:32 +0200

    add upload

--- upload.php
+++ upload.php
@@ -1,5 +1,7 @@
 <?php
-        $filename=$_FILES['photo']['name'];
+       // $filename=$_FILES['photo']['name'];
+
+        $filename = $HTTPS_POST_FILES['photo']['name']; 
         echo "Le nom du fichier contenant la photo est: ";
         echo $filename;
 ?>
