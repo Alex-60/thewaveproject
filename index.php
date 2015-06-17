1127825e068aa3be3e092f85be3db1d27b29f984
zak000 <zak.essadeq@gmail.Com>
2015-06-17 16:55:47 +0200

    database issue

--- index.php
+++ index.php
@@ -306,9 +306,12 @@
                 try
                 {
                     $dbconn3 = pg_connect("host=ec2-54-83-25-238.compute-1.amazonaws.com port=5432 dbname=dfhf24ft89btrp user=iclwqstdcanbnn password=VdN3cktdfKZZzPnasW4IxrghX6");
-                    echo "doug";
-                    echo $dbconn3;
+                    echo "-----------------";
                     
+                    
+                    $result = pg_query($dbconn3, "SELECT * FROM villes");
+                  echo $result;
+                   
                 }
                 catch (Exception $e)
                 {
