a782da5d52a7d4f10c7ce6abd6fcd3bd51c61f46
zak000 <zak.essadeq@gmail.Com>
2015-06-17 16:20:12 +0200

    database issue

--- index.php
+++ index.php
@@ -301,9 +301,9 @@
                  <?php
             
 
-                 $db = pg_connect("pgsql:host=ec2-54-83-25-238.compute-1.amazonaws.com port=5432 dbname=dfhf24ft89btrp user=iclwqstdcanbnn password=VdN3cktdfKZZzPnasW4IxrghX6") or die("connection faild");
+                // $db = pg_connect("pgsql:host=ec2-54-83-25-238.compute-1.amazonaws.com port=5432 dbname=dfhf24ft89btrp user=iclwqstdcanbnn password=VdN3cktdfKZZzPnasW4IxrghX6") or die("connection faild");
        
-               
+                $dbconn3 = pg_connect("host=ec2-54-83-25-238.compute-1.amazonaws.com port=5432 dbname=dfhf24ft89btrp user=iclwqstdcanbnn password=VdN3cktdfKZZzPnasW4IxrghX6");
 
                      ?>
                
