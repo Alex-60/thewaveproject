d2f746b83fe2d2abc3bc3125f124cecb82e30cda
zak000 <zak.essadeq@gmail.Com>
2015-06-18 00:21:59 +0200

    add upload

--- index.php
+++ index.php
@@ -267,64 +267,13 @@
     <div class="col-md-12" id="div1-child2">
         
 
-        
-      <?php 
-
-// filename: upload.form.php 
-
-// first let's set some variables 
-
-// make a note of the current working directory relative to root. 
-$directory_self = str_replace(basename($_SERVER['PHP_SELF']), '', $_SERVER['PHP_SELF']); 
-
-// make a note of the location of the upload handler script 
-$uploadHandler = 'http://' . $_SERVER['HTTP_HOST'] . $directory_self . 'upload.processor.php'; 
-
-// set a max file size for the html upload form 
-$max_file_size = 30000; // size in bytes 
-
-// now echo the html page 
-?>
-
-<html lang="en"> 
-    <head> 
-        <meta http-equiv="content-type" content="text/html; charset=iso-8859-1"> 
-     
-        <link rel="stylesheet" type="text/css" href="stylesheet.css"> 
-         
-        <title>Upload form</title> 
-     
-    </head> 
-     
-    <body> 
-     
-    <form id="Upload" action="<?php echo $uploadHandler ?>" enctype="multipart/form-data" method="post"> 
-     
-        <h1> 
-            Upload form 
-        </h1> 
-         
-        <p> 
-            <input type="hidden" name="MAX_FILE_SIZE" value="<?php echo $max_file_size ?>"> 
-        </p> 
-         
-        <p> 
-            <label for="file">File to upload:</label> 
-            <input id="file" type="file" name="file"> 
-        </p> 
-                 
-        <p> 
-            <label for="submit">Press to...</label> 
-            <input id="submit" type="submit" name="submit" value="Upload me!"> 
-        </p> 
-     
-    </form> 
-     
-     
-    </body> 
-
-</html> 
-    
+     <form enctype="multipart/form-data" action="upload.php" method="POST">
+        <!-- MAX_FILE_SIZE must precede the file input field -->
+        <input type="hidden" name="MAX_FILE_SIZE" value="30000" />
+        <!-- Name of input element determines name in $_FILES array -->
+        Send this file: <input name="userfile" type="file" />
+        <input type="submit" value="Send File" />
+</form>
         
         
                  
--- upload.php
+++ upload.php
@@ -1,7 +1,7 @@
 <?php
        // $filename=$_FILES['photo']['name'];
 
-        $filename = $HTTPS_POST_FILES['photo']['name']; 
+        $filename = $_FILES['userfile']['name']; 
         echo "Le nom du fichier contenant la photo est: ";
         echo $filename;
 ?>
