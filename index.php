493922e27d73f7e476beaed3112ad354aff3d8d8
zak000 <zak.essadeq@gmail.Com>
2015-06-17 18:05:17 +0200

    upload picture

--- index.php
+++ index.php
@@ -274,6 +274,19 @@
                 <br>
                 <input type="submit">
             </form>
+            
+            <div style="border:1px solid black;">
+            
+            
+            <form action="upload.php" method="post" enctype="multipart/form-data">
+                Select image to upload:
+                <input type="file" name="fileToUpload" id="fileToUpload">
+                <input type="submit" value="Upload Image" name="submit">
+            </form>
+
+            
+            </div>
+            
         </div>
                  
                  <?php
