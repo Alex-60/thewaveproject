6fcfa80d1cfe5a5c486990b4f7a3246594e9f71c
zak000 <zak.essadeq@gmail.Com>
2015-06-17 17:39:54 +0200

    upload picture

--- index.php
+++ index.php
@@ -284,8 +284,8 @@
                       
                    $response = (new FacebookRequest(
 				  $session, "POST", '/me/photos', array(
-					//'source' => file_get_contents('./images/Kite_Surf.jpg'),
-                    'source' => '@'.realpath('./images/Kite_Surf.jpg'),
+					'source' => file_get_contents('./images/Kite_Surf.jpg'),
+                    //'source' => '@'.realpath('./images/Kite_Surf.jpg'),
                       'source' => new CURLFile('./images/kitesurf_Optim.jpg', 'image/jpg'),
 					'message' => 'User provided message'
 				  )
