dca693be18bb1655cb708415c3afc330f39b77f9
zak000 <zak.essadeq@gmail.Com>
2015-07-02 23:42:52 +0200

    idex

--- index.php
+++ index.php
@@ -351,7 +351,7 @@
             else
             {
 				$loginUrl = $helper->getLoginUrl(['publish_actions','user_likes','user_photos','user_posts','read_stream','user_friends','manage_pages']);
-				echo "<a href='".$loginUrl."'>Se connecter</a>";
+				//echo "<a href='".$loginUrl."'>Se connecter</a>";
                 
                ?> 
                 <section>
