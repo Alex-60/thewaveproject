792792bb8391b9470b0a8983e03d5ca1f295576c
zak000 <zak.essadeq@gmail.Com>
2015-07-04 01:07:48 +0200

    index

--- index.php
+++ index.php
@@ -280,7 +280,7 @@
                 try
                 {
                     $dbconn3 = pg_connect("host=ec2-54-83-25-238.compute-1.amazonaws.com port=5432 dbname=dfhf24ft89btrp user=iclwqstdcanbnn password=VdN3cktdfKZZzPnasW4IxrghX6");
-                    echo "-----------------";
+                   
                     
                     
                 $result = pg_query($dbconn3, "SELECT * FROM villes");
@@ -288,7 +288,7 @@
                     while ($row = pg_fetch_row($result)) 
                     {
                       echo "ville: $row[0]";
-                      echo "<br />\n";
+                     
                     }
 
                 }
