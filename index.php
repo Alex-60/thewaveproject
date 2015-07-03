8fc22d5e03bb314460f290fa2513da05076664a2
zak000 <zak.essadeq@gmail.Com>
2015-07-03 13:55:02 +0200

    idex

--- index.php
+++ index.php
@@ -366,7 +366,7 @@
     <article class="slogan">
         <p>JETEZ-VOUS À L'EAU</p>
         <button class="voter">VOTER</button>
-        <a class="participer" href="">PARTICIPER</button>
+        <a class="participer" href="<?php echo "<a href='".$loginUrl."'>Se connecter</a>"; ?>"></button>
     </article>
 
     <p>Classement actuel</p>
