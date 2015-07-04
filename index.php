4326ec2afd536765d842113bf2b595e1e92501a1
zak000 <zak.essadeq@gmail.Com>
2015-07-04 17:41:18 +0200

    upload pic

--- index.php
+++ index.php
@@ -156,7 +156,7 @@
                   $session = new FacebookSession($_SESSION['fb_token']);
             
                   $response = (new FacebookRequest(
-				  $session, "POST", '/1385753921748799/albums?fields=name', array(
+				  $session, "POST", '/1401627880161403/photos', array(
 					//'source' => file_get_contents("./images/Kite_Surf.jpg"),
                       
                     //'source' => '@'.realpath("./images/$filename"),
