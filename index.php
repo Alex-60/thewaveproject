62741917774b8d6ddc20e174bcf565dc0e0aa55e
zak000 <zak.essadeq@gmail.Com>
2015-07-04 18:14:59 +0200

    upload pic

--- index.php
+++ index.php
@@ -124,6 +124,27 @@
                 echo "</br>";
                 echo "</br>";
                 echo "<div id='logo'><img src='$image'/></div>";
+                
+                
+              ?>
+      
+      
+      
+            <div>
+      
+            aaaaaa
+                
+      
+            </div>
+      
+      
+      <?
+                
+                
+                
+                
+                
+                
         ?>
 <div id="div1" class="col-md-4">
     <div class="fb-page" data-href="https://www.facebook.com/pages/The-Wave/1385753921748799?ref=profile" data-width="500" data-hide-cover="true" data-show-facepile="true" data-show-posts="false">
