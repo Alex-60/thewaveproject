9d3c3c75fe2311f7d022d226368380a51c0a79e7
zak000 <zak.essadeq@gmail.Com>
2015-07-04 18:23:33 +0200

    upload pic

--- index.php
+++ index.php
@@ -223,13 +223,11 @@
                     <?php
                 try
                 {
-                    $dbconn3 = pg_connect("host=ec2-54-83-25-238.compute-1.amazonaws.com port=5432 dbname=dfhf24ft89btrp user=iclwqstdcanbnn password=VdN3cktdfKZZzPnasW4IxrghX6");
-                    echo "-----------------";
+                   
                     
-                    
-                $result = pg_query($dbconn3, "SELECT * FROM photo");
+                $result2 = pg_query($dbconn3, "SELECT * FROM photo");
                 
-                    while ($row = pg_fetch_row($result)) 
+                    while ($row = pg_fetch_row($result2)) 
                     {
                       echo "photo: $row[0]";
                       echo "<br />\n";
