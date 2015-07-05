8aea752137bb79694812ef75b72cd0ab7d9ff6af
zak000 <zak.essadeq@gmail.Com>
2015-07-05 19:59:34 +0200

    new home

--- home.php
+++ home.php
@@ -44,22 +44,7 @@
 <section>
     
   <div id="presentation">
-   <script>
-		  window.fbAsyncInit = function() {
-		    FB.init({
-		      appId      : '<?php echo APPID; ?>',
-		      xfbml      : true,
-		      version    : 'v2.3'
-		    });
-		  };
-		  (function(d, s, id){
-		     var js, fjs = d.getElementsByTagName(s)[0];
-		     if (d.getElementById(id)) {return;}
-		     js = d.createElement(s); js.id = id;
-		     js.src = "//connect.facebook.net/fr_FR/sdk.js";
-		     fjs.parentNode.insertBefore(js, fjs);
-		   }(document, 'script', 'facebook-jssdk'));
-      </script>
+
     <br>
        <?php
 
