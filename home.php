f8f46fcc2314b592a10ed518e1ed2c7d01630b45
zak000 <zak.essadeq@gmail.Com>
2015-07-05 19:38:31 +0200

    new home

--- home.php
+++ home.php
@@ -1,4 +1,35 @@
 <!DOCTYPE html>
+
+<?php
+    error_reporting(E_ALL);
+    ini_set("display_errors", 1);
+    session_start();
+    require_once 'facebook-php-sdk-v4-4.0-dev/autoload.php';
+    use Facebook\FacebookSession;
+	use Facebook\FacebookRedirectLoginHelper;
+	use Facebook\FacebookRequest;
+	use Facebook\GraphUser;
+    use Facebook\FacebookPermissions;
+    use Facebook\FacebookPermissionException;
+    use Facebook\FacebookRequestException;
+    const APPID ="767304380051847";
+    const APPSECRET ="7f0e4cac931818f7f7dc86d722dd5e0e";
+    //$fbPermissions = 'publish_stream,user_photos';  //Required facebook permissions
+    FacebookSession::setDefaultApplication(APPID, APPSECRET);
+    $helper = new FacebookRedirectLoginHelper('https://thewave.herokuapp.com/');
+	//SI les variables de sessions existent et que $_SESSION['fb_token'] existe
+	// alors je veux créer mon utilisateur à partir de cette session
+	if( isset($_SESSION) && isset($_SESSION['fb_token']) )
+	{
+		$session = new FacebookSession($_SESSION['fb_token']);
+	}
+	//Sinon j'affiche le lien de connection
+	else
+	{
+		$session = $helper->getSessionFromRedirect();
+	}
+?>
+
 <html lang="fr">
 
 <head>
@@ -8,7 +39,21 @@
     <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
 </head>
 
-<body>
+    <?php
+
+			if($session)
+			{
+            
+            	$_SESSION['fb_token'] = (string) $session->getAccessToken();
+				$request_user = new FacebookRequest( $session,"GET","/me");
+				$request_user_executed = $request_user->execute();
+				$user = $request_user_executed- >getGraphObject(GraphUser::className());
+            
+            
+            }else{
+                
+                ?>
+                <body>
     <div class="page-home">
 
         <header class="header">
@@ -79,4 +124,12 @@
 
     </div>
 </body>
+             <?   
+            }
+                
+                
+?>
+    
+    
+
 </html>
