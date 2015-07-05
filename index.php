fea781dcbaef5c257778f9dd1da553fd074ce201
zak000 <zak.essadeq@gmail.Com>
2015-07-05 15:25:09 +0200

    index

--- index.php
+++ index.php
@@ -243,10 +243,15 @@
                                             //$result3 = pg_query($dbconn3, "INSERT INTO photo VALUES ('$picvalue')");
                                         
                                             //echo "yes";
-                                            echo "<p>$value->picture</p></br></br></br></br></br></br></br></br></br></br></br>";
+                                            echo "<p>$value->picture</p>";
                                             
-                                            
+                                            $dbconn3 = pg_connect("host=ec2-54-83-25-238.compute-1.amazonaws.com port=5432 dbname=dfhf24ft89btrp user=iclwqstdcanbnn password=VdN3cktdfKZZzPnasW4IxrghX6");
                             
+                                            $picvalue=$value->picture;
+                            
+                                            $result3 = pg_query($dbconn3, "INSERT INTO photo VALUES ('$picvalue')");
+                            
+                                            echo "<p>yes</p>";
 
                                         ?>
                                     </div>
