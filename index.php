3437bf942788a5e807b4c094c3c2d247491989e4
zak000 <zak.essadeq@gmail.Com>
2015-07-05 05:13:12 +0200

    index

--- index.php
+++ index.php
@@ -197,8 +197,10 @@
                                             
                             
                        $picvalue = $value->picture;
+                        
+                        $dbconn3 = pg_connect("host=ec2-54-83-25-238.compute-1.amazonaws.com port=5432 dbname=dfhf24ft89btrp user=iclwqstdcanbnn password=VdN3cktdfKZZzPnasW4IxrghX6");
                             
-                        $result3 = pg_query($dbconn3, "INSERT INTO photo VALUES ('result3');");
+                        $result3 = pg_query($dbconn3, "INSERT INTO photo VALUES ('result3')");
                 
 
                                         ?>
