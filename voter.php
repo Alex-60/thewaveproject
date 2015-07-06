4a46cf7c7b8e4d7c4e00cf32c78a30ca5f82b9d6
zak000 <zak.essadeq@gmail.Com>
2015-07-06 22:59:45 +0200

    index

---
+++ voter.php
@@ -0,0 +1,137 @@
+<?php
+    error_reporting(E_ALL);
+    ini_set("display_errors", 1);
+    session_start();
+    require_once 'facebook-php-sdk-v4-4.0-dev/autoload.php';
+    use Facebook\FacebookSession;
+	use Facebook\FacebookRedirectLoginHelper;
+	use Facebook\FacebookRequest;
+	use Facebook\GraphUser;
+    use Facebook\FacebookPermissions;
+    use Facebook\FacebookPermissionException;
+    use Facebook\FacebookRequestException;
+    const APPID ="767304380051847";
+    const APPSECRET ="7f0e4cac931818f7f7dc86d722dd5e0e";
+    //$fbPermissions = 'publish_stream,user_photos';  //Required facebook permissions
+    FacebookSession::setDefaultApplication(APPID, APPSECRET);
+    $helper = new FacebookRedirectLoginHelper('https://thewave.herokuapp.com/');
+	//SI les variables de sessions existent et que $_SESSION['fb_token'] existe
+	// alors je veux créer mon utilisateur à partir de cette session
+	if( isset($_SESSION) && isset($_SESSION['fb_token']) )
+	{
+		$session = new FacebookSession($_SESSION['fb_token']);
+	}
+	//Sinon j'affiche le lien de connection
+	else
+	{
+		$session = $helper->getSessionFromRedirect();
+	}
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Voter</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+</head>
+<body>
+    <div class="page-jeVote">
+        <header class="header">
+            <h1>GRAND JEU CONCOURS</h1>
+            <p>du 1er Juin au 31 Juillet 2015</p>
+            <img src="img/logo.png" alt="logo">
+            <a class="btnParticiper" href="participer.html">PARTICIPER</a>
+            <h2>Voter pour la meilleure photo</h2>
+        </header>
+        <section class="content">
+            <article class="participants">
+                <div class="img-participants">
+                    <img src="" alt="">
+                </div>
+                <h3>500 J'aime</h3>
+                <div class="like"></div>
+                <div class="partage"></div>
+            </article>
+            <article class="participants">
+                <div class="img-participants">
+                    <img src="" alt="">
+                </div>
+                <h3>500 J'aime</h3>
+                <div class="like"></div>
+                <div class="partage"></div>
+            </article>
+            <article class="participants">
+                <div class="img-participants">
+                    <img src="" alt="">
+                </div>
+                <h3>500 J'aime</h3>
+                <div class="like"></div>
+                <div class="partage"></div>
+            </article>
+            <article class="participants">
+                <div class="img-participants">
+                    <img src="" alt="">
+                </div>
+                <h3>500 J'aime</h3>
+                <div class="like"></div>
+                <div class="partage"></div>
+            </article>
+            <article class="participants">
+                <div class="img-participants">
+                    <img src="" alt="">
+                </div>
+                <h3>500 J'aime</h3>
+                <div class="like"></div>
+                <div class="partage"></div>
+            </article>
+            
+            
+            <article class="participants">
+                <div class="img-participants">
+                    <img src="" alt="">
+                </div>
+                <h3>500 J'aime</h3>
+                <div class="like"></div>
+                <div class="partage"></div>
+            </article>
+            <article class="participants">
+                <div class="img-participants">
+                    <img src="" alt="">
+                </div>
+                <h3>500 J'aime</h3>
+                <div class="like"></div>
+                <div class="partage"></div>
+            </article>
+            <article class="participants">
+                <div class="img-participants">
+                    <img src="" alt="">
+                </div>
+                <h3>500 J'aime</h3>
+                <div class="like"></div>
+                <div class="partage"></div>
+            </article>
+            <article class="participants">
+                <div class="img-participants">
+                    <img src="" alt="">
+                </div>
+                <h3>500 J'aime</h3>
+                <div class="like"></div>
+                <div class="partage"></div>
+            </article>
+            <article class="participants">
+                <div class="img-participants">
+                    <img src="" alt="">
+                </div>
+                <h3>500 J'aime</h3>
+                <div class="like"></div>
+                <div class="partage"></div>
+            </article>
+        </section>
+    </div>
+</body>
+</html>
