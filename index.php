62fea4fb78e6e65366e6feacc63a41addc514164
Alex-60 <alex.legros60@gmail.com>
2015-07-07 12:11:38 +0200

    test

--- index.php
+++ index.php
@@ -61,7 +61,7 @@
           
         <?php $loginUrl = $helper->getLoginUrl( ['publish_actions','user_likes','user_photos','user_posts','read_stream','user_friends','manage_pages']);?>
             
-         <a href='<?php echo $loginUrl;?>'>alex</a>
+         
       
         <?php    
 
@@ -89,7 +89,7 @@
          ?>   
                 
       
-            
+            <a href='<?php echo $loginUrl;?>'>alex</a>
             
             
             
