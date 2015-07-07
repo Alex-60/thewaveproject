449c0f30c58bb2165b6f7a38b223c96f9c1b7c41
zak000 <zak.essadeq@gmail.Com>
2015-07-07 16:13:54 +0200

    test2.php

--- index.php
+++ index.php
@@ -67,24 +67,23 @@
       
        <?php 
 
-            //$helper = new FacebookRedirectLoginHelper('https://thewave.herokuapp.com/test2.php');
-            //$loginUrl = $helper->getLoginUrl(['publish_actions','user_likes','user_photos','user_posts','read_stream','user_friends','manage_pages']);
+          
 
        if (isset($_GET['error']) && $_GET['error'] == 'access_denied')
             {
  
             //$helper = new FacebookRedirectLoginHelper('https://thewave.herokuapp.com/index.php');
             //$loginUrl = $helper->getLoginUrl();
-           echo "no";
-          $helper = new FacebookRedirectLoginHelper('https://thewave.herokuapp.com/index.php');
-        $loginUrl = $helper->getLoginUrl(['publish_actions','user_likes','user_photos','user_posts','read_stream','user_friends','manage_pages']);
-    
+         
 
             }
            else
             {       echo " there no error";
-                    // redirect to fb-login
+                  
             }
+
+  $helper = new FacebookRedirectLoginHelper('https://thewave.herokuapp.com/test2.php');
+    $loginUrl = $helper->getLoginUrl(['publish_actions','user_likes','user_photos','user_posts','read_stream','user_friends','manage_pages']);
 
 ?>
             <a href='<?php echo $loginUrl;?>'>alex</a>
