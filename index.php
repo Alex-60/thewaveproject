ab5852bb84c5d2e409d686bbd74453202aa5651c
zak000 <zak.essadeq@gmail.Com>
2015-07-07 21:31:45 +0200

    add new participer.php

--- index.php
+++ index.php
@@ -74,12 +74,10 @@
           
         <?php //$loginUrl = $helper->getLoginUrl( ['publish_actions','user_likes','user_photos','user_posts','read_stream','user_friends','manage_pages']);?>
             
-         
-               
-      
-      
-       <?php 
 
+
+<?php 
+/*
 
     $helper = new FacebookRedirectLoginHelper('https://thewave.herokuapp.com/voter.php');
     $loginUrl = $helper->getLoginUrl(['publish_actions','user_likes','user_photos','user_posts','read_stream','user_friends','manage_pages']);
@@ -87,10 +85,11 @@
     $helper2 = new FacebookRedirectLoginHelper('https://thewave.herokuapp.com/participer.php');
     $loginUrl2 = $helper2->getLoginUrl(['publish_actions','user_likes','user_photos','user_posts','read_stream','user_friends','manage_pages']);
 
+*/
 
 ?>
-            <a href='<?php echo $loginUrl;?>'>VOTER</a>
-            <a href='<?php echo $loginUrl2;?>'>PARTICIPER</a>
+           <!-- <a href='<?php// echo $loginUrl;?>'>VOTER</a>
+            <a href='<?php //echo $loginUrl2;?>'>PARTICIPER</a>-->
 
         </article>
 
