5ce943e54e204660ee5e3b8b00b37817f7c76e35
zak000 <zak.essadeq@gmail.Com>
2015-07-07 21:59:27 +0200

    add new participer.php

--- voter.php
+++ voter.php
@@ -42,24 +42,11 @@ $dbconn3 = pg_connect("host=ec2-54-83-25-238.compute-1.amazonaws.com port=5432 d
                         while ($row2 = pg_fetch_row($result2)) 
                             {
                                 
-                                /*echo "<article class='participants'>";
+                                echo "<article class='participants'>";
                                 echo "<div class='img-participants'>";
                                 echo "<img src='$row2[0]'/>"; 
                                 echo "</div>"; 
-                                echo "</article>";*/
-                            
-                            
-                            
-                              echo "<article class='participants'>";
-                               echo "<div class='img-participants'>";
-                                echo    "<img src='$row2[0]' alt="">";
-                               echo "</div>";
-                                echo"<h3>500 J'aime</h3>";
-                                echo "<div class='like'></div>";
-                                echo "<div class='partage'></div>";
                                 echo "</article>";
-
-            
                             }
             ?>
         
