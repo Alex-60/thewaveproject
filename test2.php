1550f50654aa7778e9c0434b4874ffb6c24d74a3
zak000 <zak.essadeq@gmail.Com>
2015-07-07 15:47:58 +0200

    test2.php

--- test2.php
+++ test2.php
@@ -1,6 +1,106 @@
-<?php
 
-echo "test";
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Voter</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+</head>
+<body>
 
-
-?>
+    <div class="page-jeVote">
+        <header class="header">
+            <h1>GRAND JEU CONCOURS</h1>
+            <p>du 1er Juin au 31 Juillet 2015</p>
+            <img src="img/logo.png" alt="logo">
+            <a class="btnParticiper" href="participer.html">PARTICIPER</a>
+            <h2>Voter pour la meilleure photo</h2>
+        </header>
+        <section class="content">
+            <article class="participants">
+                <div class="img-participants">
+                    <img src="" alt="">
+                </div>
+                <h3>500 J'aime</h3>
+                <div class="like"></div>
+                <div class="partage"></div>
+            </article>
+            <article class="participants">
+                <div class="img-participants">
+                    <img src="" alt="">
+                </div>
+                <h3>500 J'aime</h3>
+                <div class="like"></div>
+                <div class="partage"></div>
+            </article>
+            <article class="participants">
+                <div class="img-participants">
+                    <img src="" alt="">
+                </div>
+                <h3>500 J'aime</h3>
+                <div class="like"></div>
+                <div class="partage"></div>
+            </article>
+            <article class="participants">
+                <div class="img-participants">
+                    <img src="" alt="">
+                </div>
+                <h3>500 J'aime</h3>
+                <div class="like"></div>
+                <div class="partage"></div>
+            </article>
+            <article class="participants">
+                <div class="img-participants">
+                    <img src="" alt="">
+                </div>
+                <h3>500 J'aime</h3>
+                <div class="like"></div>
+                <div class="partage"></div>
+            </article>
+            
+            
+            <article class="participants">
+                <div class="img-participants">
+                    <img src="" alt="">
+                </div>
+                <h3>500 J'aime</h3>
+                <div class="like"></div>
+                <div class="partage"></div>
+            </article>
+            <article class="participants">
+                <div class="img-participants">
+                    <img src="" alt="">
+                </div>
+                <h3>500 J'aime</h3>
+                <div class="like"></div>
+                <div class="partage"></div>
+            </article>
+            <article class="participants">
+                <div class="img-participants">
+                    <img src="" alt="">
+                </div>
+                <h3>500 J'aime</h3>
+                <div class="like"></div>
+                <div class="partage"></div>
+            </article>
+            <article class="participants">
+                <div class="img-participants">
+                    <img src="" alt="">
+                </div>
+                <h3>500 J'aime</h3>
+                <div class="like"></div>
+                <div class="partage"></div>
+            </article>
+            <article class="participants">
+                <div class="img-participants">
+                    <img src="" alt="">
+                </div>
+                <h3>500 J'aime</h3>
+                <div class="like"></div>
+                <div class="partage"></div>
+            </article>
+        </section>
+    </div>
+</body>
+</html>
