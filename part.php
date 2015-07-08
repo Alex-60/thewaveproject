8ae6d6c1105848600c0ba17d776a068ca9de4dfa
zak000 <zak.essadeq@gmail.Com>
2015-07-08 18:06:37 +0200

    add part.php

--- part.php
+++ part.php
@@ -1,74 +1,31 @@
 <?php
-
-
-
+    error_reporting(E_ALL);
+    ini_set("display_errors", 1);
+    //session_start();
     require_once 'facebook-php-sdk-v4-4.0-dev/autoload.php';
-
-    define('FACEBOOK_SDK_V4_SRC_DIR', 'facebook-php-sdk-v4-4.0-dev/src/Facebook/');
-
     use Facebook\FacebookSession;
-  
-    use Facebook\FacebookRequest;
-    use Facebook\GraphUser;
+	use Facebook\FacebookRedirectLoginHelper;
+	use Facebook\FacebookRequest;
+	use Facebook\GraphUser;
+    use Facebook\FacebookPermissions;
+    use Facebook\FacebookPermissionException;
     use Facebook\FacebookRequestException;
-
-    use Facebook\FacebookRedirectLoginHelper;
-    use Facebook\FacebookRedirectLoginHelper;
-    use Facebook\FacebookSession;
-   
-    
-    $session = new FacebookSession('access token here');
-
-
-    FacebookSession::setDefaultApplication('767304380051847', '7f0e4cac931818f7f7dc86d722dd5e0e');
-
+    const APPID ="767304380051847";
+    const APPSECRET ="7f0e4cac931818f7f7dc86d722dd5e0e";
+    //$fbPermissions = 'publish_stream,user_photos';  //Required facebook permissions
+    FacebookSession::setDefaultApplication(APPID, APPSECRET);
     $helper = new FacebookRedirectLoginHelper('https://thewave.herokuapp.com/');
-    $loginUrl = $helper->getLoginUrl();
+	//SI les variables de sessions existent et que $_SESSION['fb_token'] existe
+	// alors je veux créer mon utilisateur à partir de cette session
+	if( isset($_SESSION) && isset($_SESSION['fb_token']) )
+	{
+		$session = new FacebookSession($_SESSION['fb_token']);
+	}
+	//Sinon j'affiche le lien de connection
+	else
+	{
+		$session = $helper->getSessionFromRedirect();
+	}
 
-
-    $helper = new FacebookRedirectLoginHelper();
-    try {
-      $session = $helper->getSessionFromRedirect();
-    } catch(FacebookRequestException $ex) {
-      // When Facebook returns an error
-    } catch(\Exception $ex) {
-      // When validation fails or other local issues
-    }
-    if ($session) {
-      // Logged in
-    }
-
-$request = new FacebookRequest($session, 'GET', '/me');
-$response = $request->execute();
-$graphObject = $response->getGraphObject();
-
-echo $graphObject;
-
-
+echo "yes";
 ?>
-
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-  
-</head>
-<body>
-
-<section>
-
-    
-
-    
-    
-
-    
-    
-  <div id="presentation">
-
-      
-
-    </div>
-</section>
-</body>
-</html>
-
