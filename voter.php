91e891f8bbc19cc1fd2950a6c5fc33dc260f8143
zak000 <zak.essadeq@gmail.Com>
2015-07-08 08:45:28 +0200

    add new participer.php

--- voter.php
+++ voter.php
@@ -34,37 +34,38 @@
         </header>
         
         
-                 <?php 
+            <?php 
                 
-$dbconn3 = pg_connect("host=ec2-54-83-25-238.compute-1.amazonaws.com port=5432 dbname=dfhf24ft89btrp user=iclwqstdcanbnn password=VdN3cktdfKZZzPnasW4IxrghX6");
+/*$dbconn3 = pg_connect("host=ec2-54-83-25-238.compute-1.amazonaws.com port=5432 dbname=dfhf24ft89btrp user=iclwqstdcanbnn password=VdN3cktdfKZZzPnasW4IxrghX6");
                         $result2 = pg_query($dbconn3, "SELECT * FROM photo");
                     
                         while ($row2 = pg_fetch_row($result2)) 
                             {
-                                
                                 echo "<article class='participants'>";
                                 echo "<div class='img-participants'>";
                                 echo "<img src='$row2[0]'/>"; 
                                 echo "</div>"; 
                                 echo "</article>";
-                            }
+                            }*/
             ?>
-        
-        
-        
-        
-        
+
         <section class="content">
-            
             <article class="participants">
-                <div class="img-participants">
-                    <img src="" alt="">
-                </div>
-                <h3>500 J'aime</h3>
-                <div class="like"></div>
-                <div class="partage"></div>
+                
+                <?php
+        
+                $dbconn3 = pg_connect("host=ec2-54-83-25-238.compute-1.amazonaws.com port=5432 dbname=dfhf24ft89btrp user=iclwqstdcanbnn password=VdN3cktdfKZZzPnasW4IxrghX6");
+                $result2 = pg_query($dbconn3, "SELECT * FROM photo");
+        
+               echo "<div class='img-participants'>";
+                    echo "<img src="" alt=''>";
+                echo "</div>";
+                echo "<h3>500 J'aime</h3>";
+                echo "<div class='like'></div>";
+                echo "<div class='partage'></div>";
+                
+                ?>
             </article>
-            
         </section>
         
     </div>
