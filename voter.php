7b4103c5e652f333df82e60be81b6e8fdb67d081
zak000 <zak.essadeq@gmail.Com>
2015-07-08 09:27:08 +0200

    add new participer.php

--- voter.php
+++ voter.php
@@ -48,14 +48,27 @@ $dbconn3 = pg_connect("host=ec2-54-83-25-238.compute-1.amazonaws.com port=5432 d
                                 echo "<article class='participants'>";
                                 echo "<div class='img-participants'>";
                                 echo "<img src='$row2[0]'/>"; 
-                            echo "test2";
-                              echo "<p style='color:black'>test</p>";
+                                echo "test2";
+                                echo "<p style='color:black'>test</p>";
                                 echo "</div>"; 
                                 echo "</article>";
                             }
             ?>
 
         <section class="content">
+            
+            <?php
+                
+        $dbconn3 = pg_connect("host=ec2-54-83-25-238.compute-1.amazonaws.com port=5432 dbname=dfhf24ft89btrp user=iclwqstdcanbnn password=VdN3cktdfKZZzPnasW4IxrghX6");
+                        $result2 = pg_query($dbconn3, "SELECT * FROM photo");
+                    
+                        while ($row2 = pg_fetch_row($result2)) 
+                            {
+                            echo "nop";
+                            
+                        }
+            
+            ?>
             <article class="participants">
                 
                 <div class="img-participants">
