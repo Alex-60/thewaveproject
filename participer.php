e5b332c9f75d5360f517a5d51d061bb68d22e81e
zak000 <zak.essadeq@gmail.Com>
2015-07-08 16:06:16 +0200

    add new participer.php

--- participer.php
+++ participer.php
@@ -1,7 +1,7 @@
 <?php
     error_reporting(E_ALL);
     ini_set("display_errors", 1);
-    session_start();
+
     require_once 'facebook-php-sdk-v4-4.0-dev/autoload.php';
     use Facebook\FacebookSession;
 	use Facebook\FacebookRedirectLoginHelper;
@@ -12,20 +12,11 @@
     use Facebook\FacebookRequestException;
     const APPID ="767304380051847";
     const APPSECRET ="7f0e4cac931818f7f7dc86d722dd5e0e";
-    //$fbPermissions = 'publish_stream,user_photos';  //Required facebook permissions
+
     FacebookSession::setDefaultApplication(APPID, APPSECRET);
-    $helper = new FacebookRedirectLoginHelper('https://thewave.herokuapp.com/');
-	//SI les variables de sessions existent et que $_SESSION['fb_token'] existe
-	// alors je veux créer mon utilisateur à partir de cette session
-	if( isset($_SESSION) && isset($_SESSION['fb_token']) )
-	{
-		$session = new FacebookSession($_SESSION['fb_token']);
-	}
-	//Sinon j'affiche le lien de connection
-	else
-	{
-		$session = $helper->getSessionFromRedirect();
-	}
+    $helper = new FacebookRedirectLoginHelper('https://thewave.herokuapp.com/participer.php');
+	
+	
 ?>
 
 
