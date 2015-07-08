39b66f38932ac3744980f72962e4db110c9a53ba
zak000 <zak.essadeq@gmail.Com>
2015-07-08 17:01:56 +0200

    add part.php

--- part.php
+++ part.php
@@ -50,29 +50,90 @@
 </head>
 <body>
 
+<!-- //////////////////         CODE INTEGRATION FINALE          //////////////////-->
 
+<!---->
+
+
+<!-- //////////////////         CODE ZAK          //////////////////-->
 <section>
     <div id="fb-root"></div>
     
     <div id="fb-root"></div>
-
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v2.3&appId=767304380051847";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
     
-
+    
+    
+    <script>(function(d, s, id)
+        {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) return;
+                js = d.createElement(s); js.id = id;
+                js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v2.3&appId=767304380051847";
+                fjs.parentNode.insertBefore(js, fjs);
+        }
+             (document, 'script', 'facebook-jssdk'));
+    </script>
     
   <div id="presentation">
-
+   <script>
+		  window.fbAsyncInit = function() {
+		    FB.init({
+		      appId      : '<?php echo APPID; ?>',
+		      xfbml      : true,
+		      version    : 'v2.3'
+		    });
+		  };
+		  (function(d, s, id){
+		     var js, fjs = d.getElementsByTagName(s)[0];
+		     if (d.getElementById(id)) {return;}
+		     js = d.createElement(s); js.id = id;
+		     js.src = "//connect.facebook.net/fr_FR/sdk.js";
+		     fjs.parentNode.insertBefore(js, fjs);
+		   }(document, 'script', 'facebook-jssdk'));
+      </script>
     <br>
        <?php
 
 			if($session)
 			{
+				$_SESSION['fb_token'] = (string) $session->getAccessToken();
+				$request_user = new FacebookRequest( $session,"GET","/me");
+				$request_user_executed = $request_user->execute();
+				$user = $request_user_executed->getGraphObject(GraphUser::className());
+                        
+                
+                try {
+
+				} catch(FacebookRequestException $e) 
+                {
+
+				echo "Exception occured, code: " . $e->getCode();
+				echo " with message: " . $e->getMessage();
+
+				}  
+         
+ 
                 $name = $user->getName();
-                $id=$user->getId();
+                //$id=$user->getId();
                 
                 //$image='https://graph.facebook.com/'.$id.'/picture?width=150';
                 
                 $image='https://graph.facebook.com/1399732547014087/picture?width=150';
-               
+                
+                 echo "</br>";
+                
+                echo "</br>";
+                echo "</br>";
+                echo "</br>";
+                echo "</br>";
+                echo "</br>";
                 echo "<div id='logo'><img src='$image'/></div>";
                 
        
@@ -128,7 +189,37 @@
                         $result = $graphObject->asArray();
                     
                     
-                       
+                         /*foreach ($result['data'] as $key => $value) 
+                            {
+
+                                        $query = pg_query($dbconn3, "SELECT image FROM photo where image = '$login'");
+                                        if(pg_num_rows($query) == 1)
+                                        {
+                                           // Pseudo déjà utilisé
+                                           echo 'Ce pseudo est déjà utilisé';
+                                        }else
+                                        {
+
+                                            $query = pg_query($dbconn3, " INSERT INTO photo (image) VALUES ('$login')");
+
+                                        }
+                             
+                             
+
+                                
+                            }*/
+                    
+                    
+                     //$image='https://graph.facebook.com/1399732547014087/picture?width=150'; 
+                    
+                    
+                       //$picvalue = $value->picture;
+                        
+                        //$dbconn3 = pg_connect("host=ec2-54-83-25-238.compute-1.amazonaws.com port=5432 dbname=dfhf24ft89btrp user=iclwqstdcanbnn password=VdN3cktdfKZZzPnasW4IxrghX6");
+                            
+                        //$result3 = pg_query($dbconn3, "INSERT INTO photo VALUES ('result3')");
+                    
+                    
                 }
             
             }
@@ -138,10 +229,7 @@
                 </div>
       
                 <div id="div2" class="col-md-8">
-                    
-                    
                         <?php
-                
                         //$request = new FacebookRequest($session,'GET','/1385753921748799/posts?fields=picture,full_picture');
                 
                         $request = new FacebookRequest($session,'GET','/1457732501214091/photos?fields=picture');
@@ -155,7 +243,38 @@
                         foreach ($result['data'] as $key => $value) 
                             {
                                 ?>
-                                    
+                                    <div id ="border_posts" class="col-md-6" style="text-aligne:center;">
+                                        <?php
+                                            echo "<div id='img_posts'><img src='$value->picture' /></div></br></br></br></br></br>";
+                            
+                                            //$dbconn3 = pg_connect("host=ec2-54-83-25-238.compute-1.amazonaws.com port=5432 dbname=dfhf24ft89btrp user=iclwqstdcanbnn password=VdN3cktdfKZZzPnasW4IxrghX6");
+                            
+                                            //$picvalue=$value->picture;
+                            
+                                            //$result3 = pg_query($dbconn3, "INSERT INTO photo VALUES ('$picvalue')");
+                                        
+                                            //echo "yes";
+                                            echo "<p>$value->picture</p>";
+                                            
+                                            $dbconn3 = pg_connect("host=ec2-54-83-25-238.compute-1.amazonaws.com port=5432 dbname=dfhf24ft89btrp user=iclwqstdcanbnn password=VdN3cktdfKZZzPnasW4IxrghX6");
+                            
+                                            //$picvalue=$value->picture;
+
+                                            //if $picvalue existe dans la base de données on l'ajoute pas 
+                            
+                                            $result3 = pg_query($dbconn3, "INSERT INTO photo VALUES ('$picvalue')");
+                            
+                                            echo "<p>yes</p>";
+                            
+                                            ?>
+                                        
+                                                <div class="fb-like" data-href="https://scontent.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/s130x130/11012110_1458080871179254_8388735200119380577_n.jpg?oh=d3cfc6640164cc059d501f72abdb64fa&amp;oe=561632DF" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
+                                        
+                                        
+                                            <?php
+
+                                        ?>
+                                    </div>
                                    
                                 <?php
                             }
@@ -192,12 +311,41 @@
        
         
         <hr>
-  
+         <?php
+                try
+                {
+                    $dbconn3 = pg_connect("host=ec2-54-83-25-238.compute-1.amazonaws.com port=5432 dbname=dfhf24ft89btrp user=iclwqstdcanbnn password=VdN3cktdfKZZzPnasW4IxrghX6");
+                    $result = pg_query($dbconn3, "SELECT * FROM villes");
+                
+                    while ($row = pg_fetch_row($result)) 
+                    {
+                      echo "ville: $row[0]";
+                     
+                    }
+                    
+                }
+                catch (Exception $e)
+                {
+                var_dump($e->getMessage());
+                }
+        ?>
     </article>
 
     <p></p>
     <p>Classement actuel</p>
-
+    <article class="classement">
+       
+        <?php
+        
+         $result2 = pg_query($dbconn3, "SELECT * FROM photo");
+                
+                    while ($row2 = pg_fetch_row($result2)) 
+                    {
+                   
+                         echo "<img src='$row2[0]'/>";
+                    }
+        ?>
+    </article>
 
     <footer>
             <p>Conditions générales : The Wave - jeu concours est une marque déposée par des étudiants de l'École Supérieur de Génie Informatique (ESGI), dans le cadre d'un projet scolaire</p>
