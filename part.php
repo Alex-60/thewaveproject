d089e371911d20176796d150b7d7d66e1c0b8eb3
zak000 <zak.essadeq@gmail.Com>
2015-07-08 18:22:04 +0200

    add part.php

--- part.php
+++ part.php
@@ -31,7 +31,18 @@
 
 echo "yes";
 
-echo $session;
 
-echo "no";
+if($session) {
+  try {
+    $user_profile = (new FacebookRequest(
+      $session, 'GET', '/me'
+    ))->execute()->getGraphObject(GraphUser::className());
+    echo "Name: " . $user_profile->getName();
+  } catch(FacebookRequestException $e) {
+    echo "Exception occured, code: " . $e->getCode();
+    echo " with message: " . $e->getMessage();
+  }   
+}
+
+
 ?>
