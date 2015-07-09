64b05c310484eab72c9425fe13e07f6d65c93345
zak000 <zak.essadeq@gmail.Com>
2015-07-09 11:37:01 +0200

    add part.php

--- voter.php
+++ voter.php
@@ -44,6 +44,8 @@
         
         $loginUrl = $helper->getLoginUrl(['publish_actions','user_likes','user_photos','user_posts','read_stream','user_friends','manage_pages']);
         
+        var_dump($loginUrl);
+die();
             {    ?>   
                
 <!DOCTYPE html>
