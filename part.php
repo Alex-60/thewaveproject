b2ee9357b2df26d6f6551848ff3cbf25b2c38efe
zak000 <zak.essadeq@gmail.Com>
2015-07-09 11:11:54 +0200

    add part.php

--- part.php
+++ part.php
@@ -1,51 +1,7 @@
 <?php
 
    
-    session_start();
-
-    require_once 'facebook-php-sdk-v4-4.0-dev/autoload.php';
-
-    use Facebook\FacebookSession;
-	use Facebook\FacebookRedirectLoginHelper;
-	use Facebook\FacebookRequest;
-	use Facebook\GraphUser;
-    use Facebook\FacebookPermissions;
-    use Facebook\FacebookPermissionException;
-    use Facebook\FacebookRequestException;
-
-    const APPID ="767304380051847";
-    const APPSECRET ="7f0e4cac931818f7f7dc86d722dd5e0e";
-
-    
-    FacebookSession::setDefaultApplication(APPID, APPSECRET);
-    $helper = new FacebookRedirectLoginHelper('https://thewave.herokuapp.com/');
-	
-	
-
-// see if a existing session exists
-if (isset($_SESSION) && isset($_SESSION['fb_token'])) {
-    // create new session from saved access_token
-    $session = new FacebookSession($_SESSION['fb_token']);
-    // validate the access_token to make sure it's still valid
-    try {
-        if (!$session->validate()) {
-            $session = null;
-        }
-    } catch (Exception $e) {
-        // catch any exceptions
-        $session = null;
-    }
-} else {
-    // no session exists
-    try {
-        $session = $helper->getSessionFromRedirect();
-    } catch (FacebookRequestException $ex) {
-        // When Facebook returns an error
-    } catch (Exception $ex) {
-        // When validation fails or other local issues
-        echo $ex->message;
-    }
-}
+  include_once "index.php";
 
 
 ?>
