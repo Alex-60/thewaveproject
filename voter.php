48e3402fe834d9223db1a9ce8ae63bcbaa9a290d
zak000 <zak.essadeq@gmail.Com>
2015-07-09 23:30:26 +0200

    add home.php

--- voter.php
+++ voter.php
@@ -13,7 +13,25 @@
     else
         
   
- 
+        
+        
+    session_start();
+
+    use Facebook\FacebookSession;
+	use Facebook\FacebookRedirectLoginHelper;
+    require_once 'facebook-php-sdk-v4-4.0-dev/autoload.php';
+
+      
+
+    $APPID ="767304380051847";
+    $APPSECRET ="7f0e4cac931818f7f7dc86d722dd5e0e";
+    
+    FacebookSession::setDefaultApplication(APPID, APPSECRET);
+    $helper2 = new FacebookRedirectLoginHelper('https://thewave.herokuapp.com/participer.php');
+   
+
+    echo $helper2->getLoginUrl();
+
    {    ?>   
                
 <!DOCTYPE html>
