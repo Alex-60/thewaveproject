188c780d4b2c31593204f403c9c61da33f2ff364
zak000 <zak.essadeq@gmail.Com>
2015-07-09 11:48:20 +0200

    add part.php

--- voter.php
+++ voter.php
@@ -10,48 +10,8 @@
     }
     else
         
-        
-    error_reporting(E_ALL);
-    ini_set("display_errors", 1);
-    session_start();
-    require_once 'facebook-php-sdk-v4-4.0-dev/autoload.php';
-    use Facebook\FacebookSession;
-	use Facebook\FacebookRedirectLoginHelper;
-	use Facebook\FacebookRequest;
-	use Facebook\GraphUser;
-    use Facebook\FacebookPermissions;
-    use Facebook\FacebookPermissionException;
-    use Facebook\FacebookRequestException;
-    const APPID ="767304380051847";
-    const APPSECRET ="7f0e4cac931818f7f7dc86d722dd5e0e";
-    //$fbPermissions = 'publish_stream,user_photos';  //Required facebook permissions
-    FacebookSession::setDefaultApplication(APPID, APPSECRET);
-    $helper = new FacebookRedirectLoginHelper('https://thewave.herokuapp.com/');
-	//SI les variables de sessions existent et que $_SESSION['fb_token'] existe
-	// alors je veux créer mon utilisateur à partir de cette session
-	if( isset($_SESSION) && isset($_SESSION['fb_token']) )
-	{
-		$session = new FacebookSession($_SESSION['fb_token']);
-	}
-	//Sinon j'affiche le lien de connection
-	else
-	{
-		$session = $helper->getSessionFromRedirect();
-	}
-
-
 
         
-        $loginUrl = $helper->getLoginUrl(['publish_actions','user_likes','user_photos','user_posts','read_stream','user_friends','manage_pages']);
-
-
-
-           $request = new FacebookRequest($session,'GET','/1457732501214091/photos?fields=picture');
-                
-                        $response = $request->execute();
-                        $graphObject = $response->getGraphObject(GraphUser::className());
-                        $result = $graphObject->asArray();
-                
         
         
    {    ?>   
