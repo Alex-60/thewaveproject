d29c9fce000c0246504f6b3bdf2c8497010a0f90
zak000 <zak.essadeq@gmail.Com>
2015-07-10 20:32:12 +0200

    new test

--- fbconfig.php
+++ fbconfig.php
@@ -1,7 +1,9 @@
 <?php
 session_start();
 // added in v4.0.0
-require_once 'autoload.php';
+//require_once 'autoload.php';
+require_once 'facebook-php-sdk-v4-4.0-dev/autoload.php';
+
 use Facebook\FacebookSession;
 use Facebook\FacebookRedirectLoginHelper;
 use Facebook\FacebookRequest;
