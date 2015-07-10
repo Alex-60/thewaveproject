cec38b458e8c91973badcd3f9df5568afe67f476
zak000 <zak.essadeq@gmail.Com>
2015-07-10 09:07:46 +0200

    fonction.php

--- index.php
+++ index.php
@@ -1,15 +1,19 @@
 <?php
-    error_reporting(E_ALL);
-    ini_set("display_errors", 1);
-    session_start();
+    //error_reporting(E_ALL);
+    //ini_set("display_errors", 1);
+    
     require_once 'facebook-php-sdk-v4-4.0-dev/autoload.php';
+
+session_start();
+
+
     use Facebook\FacebookSession;
 	use Facebook\FacebookRedirectLoginHelper;
-	use Facebook\FacebookRequest;
-	use Facebook\GraphUser;
-    use Facebook\FacebookPermissions;
-    use Facebook\FacebookPermissionException;
-    use Facebook\FacebookRequestException;
+	//use Facebook\FacebookRequest;
+	//use Facebook\GraphUser;
+    //use Facebook\FacebookPermissions;
+    //use Facebook\FacebookPermissionException;
+    //use Facebook\FacebookRequestException;
     const APPID ="767304380051847";
     const APPSECRET ="7f0e4cac931818f7f7dc86d722dd5e0e";
 
