81f362bcfef810312a834c520e8e491518979e17
zak000 <zak.essadeq@gmail.Com>
2015-07-10 21:22:49 +0200

    new test

--- fbconfig.php
+++ fbconfig.php
@@ -16,24 +16,21 @@ require_once 'facebook-php-sdk-v4-4.0-dev/autoload.php';
 // init app with app id and secret
 FacebookSession::setDefaultApplication( '767304380051847','7f0e4cac931818f7f7dc86d722dd5e0e' );
 // login helper with redirect_uri
-    $helper = new FacebookRedirectLoginHelper('https://thewave.herokuapp.com');
+    $helper = new FacebookRedirectLoginHelper('');
 try 
 {
   $session = $helper->getSessionFromRedirect();
 } catch( FacebookRequestException $ex ) 
 {
-    echo "1";
+    
   // When Facebook returns an error
 } catch( Exception $ex ) 
 {
   // When validation fails or other local issues
-    echo "2";
 }
 // see if we have a session
 if ( isset( $session ) ) 
 {
-    
-    echo "yes index";
   // graph api request for user data
   $request = new FacebookRequest( $session, 'GET', '/me' );
   $response = $request->execute();
