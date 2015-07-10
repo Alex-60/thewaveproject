e0627bcb0f5979f5152ef88484611efcce4c26af
zak000 <zak.essadeq@gmail.Com>
2015-07-10 08:35:46 +0200

    fonction.php

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
 		$session = $helper->getSessionFromRedirect();
 	}
 
-    echo $helper->getLoginUrl();
+    echo $helper->getLoginUrl(['publish_actions','user_likes','user_photos','user_posts','read_stream','user_friends','manage_pages']);
 ?>
 
 <!DOCTYPE html>
