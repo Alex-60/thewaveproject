e98fbd5164fdd13b30ac7024a6a7975610e334fb
zak000 <zak.essadeq@gmail.Com>
2015-07-10 10:34:16 +0200

    fonction.php

--- index.php
+++ index.php
@@ -70,11 +70,7 @@ session_start();
     $helper2 = new FacebookRedirectLoginHelper('https://thewave.herokuapp.com/voter.php');
     $loginUrl = $helper2->getLoginUrl(['publish_actions','user_likes','user_photos','user_posts','read_stream','user_friends','manage_pages']);
 
-    $session2 = $helper2->getSessionFromRedirect();
 
-    $_SESSION['pseudo']=$session2; 
-
-    //var_dump($session2);
 
 
 
