b094fab09d6e973dc4512e033501ab81ba08d242
zak000 <zak.essadeq@gmail.Com>
2015-07-11 01:44:46 +0200

    new files

--- fbconfig.php
+++ fbconfig.php
@@ -1,21 +1,13 @@
 <?php
 
-// added in v4.0.0
-//require_once 'autoload.php';
-/*require_once 'facebook-php-sdk-v4-4.0-dev/autoload.php';
-    use Facebook\FacebookSession;
-	use Facebook\FacebookRedirectLoginHelper;
-	use Facebook\FacebookRequest;
-	use Facebook\GraphUser;
-    use Facebook\FacebookPermissions;
-    use Facebook\FacebookPermissionException;
-    use Facebook\FacebookRequestException;*/
-
-
 session_start();
 // added in v4.0.0
+
 require_once 'autoload.php';
 
+
+//require_once 'index.php';
+  
 use Facebook\FacebookSession;
 use Facebook\FacebookRedirectLoginHelper;
 use Facebook\FacebookRequest;
@@ -28,21 +20,13 @@ use Facebook\Entities\AccessToken;
 use Facebook\HttpClients\FacebookCurlHttpClient;
 use Facebook\HttpClients\FacebookHttpable;
 
-
-
-
 // init app with app id and secret
 FacebookSession::setDefaultApplication( '767304380051847','7f0e4cac931818f7f7dc86d722dd5e0e' );
 // login helper with redirect_uri
-    $helper = new FacebookRedirectLoginHelper("https://thewave.herokuapp.com");
-try 
-{
+    $helper = new FacebookRedirectLoginHelper('https://thewave.herokuapp.com/');
+try {
   $session = $helper->getSessionFromRedirect();
-    
- 
-} catch( FacebookRequestException $ex ) 
-{
-    
+} catch( FacebookRequestException $ex ) {
   // When Facebook returns an error
 } catch( Exception $ex ) 
 {
@@ -51,31 +35,25 @@ try
 // see if we have a session
 if ( isset( $session ) ) 
 {
-    
-    echo "yes";
-    
-    die();
-    
   // graph api request for user data
   $request = new FacebookRequest( $session, 'GET', '/me' );
   $response = $request->execute();
   // get response
   $graphObject = $response->getGraphObject();
+  $fbid = $graphObject->getProperty('id');
     
-        
-        
-        $fbid = $graphObject->getProperty('id');              // To Get Facebook ID
+    // To Get Facebook ID
  	    $fbfullname = $graphObject->getProperty('name'); // To Get Facebook full name
 	    $femail = $graphObject->getProperty('email');    // To Get Facebook email ID
 	/* ---- Session Variables -----*/
-	    $_SESSION['FBID'] = $fbid;           
+	    $_SESSION['FBID'] = $fbid;  
         $_SESSION['FULLNAME'] = $fbfullname;
 	    $_SESSION['EMAIL'] =  $femail;
-    /* ---- header location after session ----*/
-  header("Location: index.php");
+  //checkuser($fuid,$ffname,$femail);
+  //header("Location: index.php");
 } else 
 {
- $loginUrl = $helper->getLoginUrl();
+  $loginUrl = $helper->getLoginUrl();
  header("Location: ".$loginUrl);
 }
 ?>
--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
-session_start(); 
+require_once 'fbconfig.php';
+
+//session_start(); 
 ?>
 <!doctype html>
 <html xmlns:fb="http://www.facebook.com/2008/fbml">
@@ -11,19 +13,19 @@ session_start();
   <?php if ($_SESSION['FBID']): ?>      <!--  After user login  -->
 <div class="container">
 <div class="hero-unit">
-  <h1>Hello <?php echo $_SESSION['USERNAME']; ?></h1>
+  <h1>Hello <?php echo $_SESSION['FULLNAME']; ?></h1>
   <p>Welcome to "facebook login" tutorial</p>
   </div>
 <div class="span4">
  <ul class="nav nav-list">
 <li class="nav-header">Image</li>
+     
+
 	<li><img src="https://graph.facebook.com/<?php echo $_SESSION['FBID']; ?>/picture"></li>
 <li class="nav-header">Facebook ID</li>
 <li><?php echo  $_SESSION['FBID']; ?></li>
 <li class="nav-header">Facebook fullname</li>
 <li><?php echo $_SESSION['FULLNAME']; ?></li>
-<li class="nav-header">Facebook Email</li>
-<li><?php echo $_SESSION['EMAIL']; ?></li>
 <div><a href="logout.php">Logout</a></div>
 </ul></div></div>
     <?php else: ?>     <!-- Before login --> 
@@ -32,8 +34,6 @@ session_start();
            Not Connected
 <div>
       <a href="fbconfig.php">Login with Facebook</a></div>
-	 <div> <a href="http://www.krizna.com/general/login-with-facebook-using-php/"  title="Login with facebook">View Post</a>
-	  </div>
       </div>
     <?php endif ?>
   </body>
