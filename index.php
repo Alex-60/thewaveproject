c387e52aea059cfb75d481adcb9073b76d3b6447
zak000 <zak.essadeq@gmail.Com>
2015-07-10 15:34:43 +0200

    fonction.php

--- index.php
+++ index.php
@@ -29,7 +29,12 @@
     
    var_dump ($_SESSION);
 
-
+FacebookRequest($_SESSION,'GET','/1457732501214091/photos?fields=picture');
+                        $response = $request->execute();
+                        $graphObject = $response->getGraphObject(GraphUser::className());
+                        $result = $graphObject->asArray();
+                            var_dump ($result);
+                    
 ?>
 
 <!DOCTYPE html>
