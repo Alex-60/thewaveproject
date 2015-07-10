9be46cf31957545b8e0731094f0103c20fc39ac6
zak000 <zak.essadeq@gmail.Com>
2015-07-10 10:13:29 +0200

    fonction.php

--- index.php
+++ index.php
@@ -66,6 +66,7 @@ session_start();
 
     $helper = new FacebookRedirectLoginHelper('https://thewave.herokuapp.com/voter.php');
     $loginUrl = $helper->getLoginUrl(['publish_actions','user_likes','user_photos','user_posts','read_stream','user_friends','manage_pages']);
+    
 
 
     $helper2 = new FacebookRedirectLoginHelper('https://thewave.herokuapp.com/participer.php');
