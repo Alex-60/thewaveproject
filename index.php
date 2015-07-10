54ce22daf4dc3a84e9d1920990c3538ad7d47853
zak000 <zak.essadeq@gmail.Com>
2015-07-10 08:12:07 +0200

    fonction.php

--- index.php
+++ index.php
@@ -1,33 +1,5 @@
 <?php
-    error_reporting(E_ALL);
-    ini_set("display_errors", 1);
-    session_start();
-    require_once 'facebook-php-sdk-v4-4.0-dev/autoload.php';
-    use Facebook\FacebookSession;
-	use Facebook\FacebookRedirectLoginHelper;
-	use Facebook\FacebookRequest;
-	use Facebook\GraphUser;
-    use Facebook\FacebookPermissions;
-    use Facebook\FacebookPermissionException;
-    use Facebook\FacebookRequestException;
-    const APPID ="767304380051847";
-    const APPSECRET ="7f0e4cac931818f7f7dc86d722dd5e0e";
-
-    FacebookSession::setDefaultApplication(APPID, APPSECRET);
-    $helper = new FacebookRedirectLoginHelper('https://thewave.herokuapp.com/');
-
-	
-    if( isset($_SESSION) && isset($_SESSION['fb_token']) )
-	{
-		$session = new FacebookSession($_SESSION['fb_token']);
-	}
-	//Sinon j'affiche le lien de connection
-	else
-	{
-		$session = $helper->getSessionFromRedirect();
-	}
-
-    //echo $helper->getLoginUrl();
+  require_once('fonction.php');
 ?>
 
 <!DOCTYPE html>
