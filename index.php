c407e04ab6c02d4e0382cc83bfaddfaa717c88d3
zak000 <zak.essadeq@gmail.Com>
2015-07-11 05:04:58 +0200

    new files

--- index.php
+++ index.php
@@ -5,10 +5,62 @@ require_once 'fbconfig.php';
 ?>
 <!doctype html>
 <html xmlns:fb="http://www.facebook.com/2008/fbml">
-  <head>
-    <title>Login with Facebook</title>
-<link href="http://www.bootstrapcdn.com/twitter-bootstrap/2.2.2/css/bootstrap-combined.min.css" rel="stylesheet"> 
- </head>
+
+<head>
+
+    <meta charset="UTF-8">
+    <title>Grand jeu concours THE WAVE</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+    
+</head>
+
+    <section>
+<body>
+<div class="page-home">
+
+        <header class="header">
+            <h1>GRAND JEU CONCOURS</h1>
+            <p>du 1er Juin au 31 Juillet 2015</p>
+            <p class="txt2">Participer et tentez de gagner<br/> votre équipement de kyte surf avec</p>
+            <img class="logohome" src="img/logo.png" />
+        </header>
+
+        <article class="slogan">
+            <p>JETEZ-VOUS À L'EAU</p>
+            <a href='' class="btnVoter">VOTER</a>
+          
+           <div class="btn-jeux">
+            <a href='' class="btnParticiper">PARTICIPER</a>
+            </div>
+
+        </article>
+
+        <section class="classement">
+            <p>TOP 5</p>
+            <?php 
+                
+                        $dbconn3 = pg_connect("host=ec2-54-83-25-238.compute-1.amazonaws.com port=5432 dbname=dfhf24ft89btrp user=iclwqstdcanbnn password=VdN3cktdfKZZzPnasW4IxrghX6");
+                        $result2 = pg_query($dbconn3, "SELECT * FROM photo ORDER BY date DESC LIMIT 5");
+                    
+                        while ($row2 = pg_fetch_row($result2)) 
+                            {
+                                
+                                echo "<article class='participants'>";
+                                echo "<div class='img-participants'>";
+                                echo "<img src='$row2[0]'/>"; 
+                                echo "</div>"; 
+                                echo "</article>";
+                            }
+            ?>
+        </section>
+
+        <footer>Conditions générales : The Wave - jeu concours est une marque déposée par des étudiants de l'École Supérieur de Génie Informatique (ESGI), dans le cadre d'un projet scolaire
+        </footer>
+
+    </div>
+</body>
+</section>
     
  <!--   
 <body>
