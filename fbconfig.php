d47f04a518169345c6201a916aa130b57ad529c5
zak000 <zak.essadeq@gmail.Com>
2015-07-11 15:12:51 +0200

    home.index

--- fbconfig.php
+++ fbconfig.php
@@ -20,6 +20,10 @@ use Facebook\Entities\AccessToken;
 use Facebook\HttpClients\FacebookCurlHttpClient;
 use Facebook\HttpClients\FacebookHttpable;
 
+echo "7baaas";
+
+die();
+
 // init app with app id and secret
 FacebookSession::setDefaultApplication( '767304380051847','7f0e4cac931818f7f7dc86d722dd5e0e' );
 // login helper with redirect_uri
