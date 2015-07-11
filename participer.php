ab0d7fb7c740aecb0364b306a91b8e922adf2ebf
zak000 <zak.essadeq@gmail.Com>
2015-07-11 16:53:18 +0200

    voter.index

--- participer.php
+++ participer.php
@@ -27,6 +27,40 @@ require_once 'fbconfig-participer.php';
     <script src="js/dnd.js"></script>
 </head>
 <body>
+    
+    
+    
+    
+    
+<div class="container">
+    <div class="hero-unit">
+      <h1>Hello <?php echo $_SESSION['FULLNAME']; ?></h1>
+      <p>Welcome to "facebook login" tutorial</p>
+    </div>
+    
+    <?php if ($_SESSION['FBID']): ?>      
+    
+    
+    <div class="span4">
+        <ul class="nav nav-list">
+                <li class="nav-header">Image</li>
+                <li><img src="https://graph.facebook.com/<?php echo $_SESSION['FBID']; ?>/picture"></li>
+                <li class="nav-header">Facebook ID</li>
+                <li><?php echo  $_SESSION['FBID']; ?></li>
+                <li class="nav-header">Facebook fullname</li>
+                <li><?php echo $_SESSION['FULLNAME']; ?></li>
+                <div><a href="logout.php">Logout</a></div>
+
+                <li class="nav-header">situation</li>
+                <li><?php echo  $_SESSION['fatigué']; ?></li>
+        </ul>
+    </div>
+    
+    <?php else: ?>   
+    
+</div>
+    
+    
     <div class="page-jeParticipe">
         <header class="header">
             <h1>GRAND JEU CONCOURS</h1>
