d7392c56d5623cfdab42d08ff5fb659ee993654b
zak000 <zak.essadeq@gmail.Com>
2015-07-11 23:43:08 +0200

    voter.index

--- participer.php
+++ participer.php
@@ -33,32 +33,29 @@ require_once 'fbconfig-participer.php';
     
     
 <div class="container">
-    <div class="hero-unit">
-      <h1>Hello <?php echo $_SESSION['FULLNAME']; ?></h1>
-      <p>Welcome to "facebook login" tutorial</p>
-    </div>
-    
-    <?php if ($_SESSION['FBID']): ?>      
-    
-    
-    <div class="span4">
-        <ul class="nav nav-list">
-                <li class="nav-header">Image</li>
-                <li><img src="https://graph.facebook.com/<?php echo $_SESSION['FBID']; ?>/picture"></li>
-                <li class="nav-header">Facebook ID</li>
-                <li><?php echo  $_SESSION['FBID']; ?></li>
-                <li class="nav-header">Facebook fullname</li>
-                <li><?php echo $_SESSION['FULLNAME']; ?></li>
-                <div><a href="logout.php">Logout</a></div>
+<div class="hero-unit">
+  <h1>Hello <?php echo $_SESSION['FULLNAME']; ?></h1>
+  <p>Welcome to "facebook login" tutorial</p>
+  </div>
+<div class="span4">
+ <ul class="nav nav-list">
+<li class="nav-header">Image</li>
+     
 
-                <li class="nav-header">situation</li>
-                <li><?php echo  $_SESSION['fatigué']; ?></li>
-        </ul>
+<li><img src="https://graph.facebook.com/<?php echo $_SESSION['FBID']; ?>/picture"></li>
+<li class="nav-header">Facebook ID</li>
+<li><?php echo  $_SESSION['FBID']; ?></li>
+<li class="nav-header">Facebook fullname</li>
+<li><?php echo $_SESSION['FULLNAME']; ?></li>
+<div><a href="logout.php">Logout</a></div>
+     
+<li class="nav-header">situation</li>
+<li><?php echo  $_SESSION['fatigué']; ?></li>
+</ul>
+    
     </div>
     
-    <?php else: ?>   
-    
-</div>
+    </div>
     
     
     <div class="page-jeParticipe">
