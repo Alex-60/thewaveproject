301544525db95e2a047da3a25244ec83f3672c19
zak000 <zak.essadeq@gmail.Com>
2015-07-11 15:29:46 +0200

    home.index

--- fbconfig.php
+++ fbconfig.php
@@ -21,15 +21,17 @@ use Facebook\HttpClients\FacebookCurlHttpClient;
 use Facebook\HttpClients\FacebookHttpable;
 
 
+echo "yes";
+    die();
+
 
 // init app with app id and secret
 FacebookSession::setDefaultApplication( '767304380051847','7f0e4cac931818f7f7dc86d722dd5e0e' );
 // login helper with redirect_uri
     $helper = new FacebookRedirectLoginHelper('https://thewave.herokuapp.com/');
 
-  echo $helper;
-echo "yes";
-    die();
+
+
 
 
 try 
