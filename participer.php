5623f3542935769b8ac9b39c94a44b6bfb174f89
zak000 <zak.essadeq@gmail.Com>
2015-07-12 15:08:39 +0200

    voter.index

--- participer.php
+++ participer.php
@@ -1,5 +1,7 @@
 <?php
 
+
+
 require_once 'fbconfig-participer.php';
 
 
@@ -34,9 +36,52 @@ require_once 'fbconfig-participer.php';
     
     <!-------------------------------------------------------------------------------------------------------------------------------------------->
 
+    
+    
+    
+   <?php  error_reporting(E_ALL);
+    ini_set("display_errors", 1);
+    session_start();
+    require_once 'facebook-php-sdk-v4-4.0-dev/autoload.php';
+    use Facebook\FacebookSession;
+	use Facebook\FacebookRedirectLoginHelper;
+	use Facebook\FacebookRequest;
+	use Facebook\GraphUser;
+    use Facebook\FacebookPermissions;
+    use Facebook\FacebookPermissionException;
+    use Facebook\FacebookRequestException;
+    const APPID ="767304380051847";
+    const APPSECRET ="7f0e4cac931818f7f7dc86d722dd5e0e";
+    //$fbPermissions = 'publish_stream,user_photos';  //Required facebook permissions
+    FacebookSession::setDefaultApplication(APPID, APPSECRET);
+    $helper = new FacebookRedirectLoginHelper('https://thewave.herokuapp.com/home.php');
+	//SI les variables de sessions existent et que $_SESSION['fb_token'] existe
+	// alors je veux créer mon utilisateur à partir de cette session
+	if( isset($_SESSION) && isset($_SESSION['fb_token']) )
+	{
+		$session = new FacebookSession($_SESSION['fb_token']);
+	}
+	//Sinon j'affiche le lien de connection
+	else
+	{
+		$session = $helper->getSessionFromRedirect();
+	}
+             
+             
+             
+             
+             if($session)
+			{
+                 
+             }
+             else {echo "nooo";
+                  }
+    ?>
+    
+    
 <html>
  <body>
-  <form action="upload.php" method="post"
+  <form action="" method="post"
     enctype="multipart/form-data">
    <label for="file">Filename:</label>
      <input type="file" name="file" id="file"><br>
