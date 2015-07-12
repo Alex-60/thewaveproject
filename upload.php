102c96bf63fddde148a151538b7855c6c740e61f
zak000 <zak.essadeq@gmail.Com>
2015-07-12 02:57:27 +0200

    upload

--- upload.php
+++ upload.php
@@ -1,7 +1,11 @@
 <?php
        // $filename=$_FILES['photo']['name'];
 
-        $filename = $_FILES['userfile']['name']; 
-        echo "Le nom du fichier contenant la photo est: ";
-        echo $filename;
+        //$filename = $_FILES['userfile']['name']; 
+        //echo "Le nom du fichier contenant la photo est: ";
+        //echo $filename;
+
+echo "Prénom tapé par l'utilisateur : ".$_POST['prenom'];
+
+
 ?>
