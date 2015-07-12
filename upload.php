16efac370b6c7b1558ece8e5dbebaf6617fa5d96
zak000 <zak.essadeq@gmail.Com>
2015-07-12 14:59:23 +0200

    voter.index

--- upload.php
+++ upload.php
@@ -51,12 +51,12 @@ session_start();
         
         
         // init app with app id and secret
-FacebookSession::setDefaultApplication( '767304380051847','7f0e4cac931818f7f7dc86d722dd5e0e' );
+FacebookSession::setDefaultApplication( '767304380051847','7f0e4cac931818f7f7dc86d722dd5e0e');
 // login helper with redirect_uri
-    $helper = new FacebookRedirectLoginHelper('https://thewave.herokuapp.com/upload.php');
+    //$helper = new FacebookRedirectLoginHelper('https://thewave.herokuapp.com/upload.php');
 try 
 {
-  $session = $helper->getSessionFromRedirect();
+  //$session = $helper->getSessionFromRedirect();
 } catch( FacebookRequestException $ex ) 
 {
   // When Facebook returns an error
