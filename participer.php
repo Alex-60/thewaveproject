3a2f9c7333d51773fe871e09e1dd8b56d9c28ecb
zak000 <zak.essadeq@gmail.Com>
2015-07-12 23:01:32 +0200

    teste file

--- participer.php
+++ participer.php
@@ -2,6 +2,28 @@
 
 //require_once 'fbconfig-participer.php';
 
+
+
+       session_start();
+                                // added in v4.0.0
+
+                                require_once 'autoload.php';
+
+                                //require_once 'index.php';
+
+                                use Facebook\FacebookSession;
+                                use Facebook\FacebookRedirectLoginHelper;
+                                use Facebook\FacebookRequest;
+                                use Facebook\FacebookResponse;
+                                use Facebook\FacebookSDKException;
+                                use Facebook\FacebookRequestException;
+                                use Facebook\FacebookAuthorizationException;
+                                use Facebook\GraphObject;
+                                use Facebook\Entities\AccessToken;
+                                use Facebook\HttpClients\FacebookCurlHttpClient;
+                                use Facebook\HttpClients\FacebookHttpable;
+
+
 ?>
 
 
@@ -60,6 +82,28 @@
 
 
                         echo "yes";
+
+                         
+
+                                // init app with app id and secret
+                                    FacebookSession::setDefaultApplication( '767304380051847','7f0e4cac931818f7f7dc86d722dd5e0e' );
+                                // login helper with redirect_uri
+                                    $helper = new FacebookRedirectLoginHelper('https://thewave.herokuapp.com/participer.php');
+                                try 
+                                {
+                                  $session = $helper->getSessionFromRedirect();
+
+                                } catch( FacebookRequestException $ex ) 
+                                {
+                                  // When Facebook returns an error
+                                } catch( Exception $ex ) 
+                                {
+                                  // When validation fails or other local issues
+                                }
+                 
+                                echo "code valide";
+                 
+                                
 
 
                     }
