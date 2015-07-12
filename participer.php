d90f9fcb956af8a8c304c2fbb58a9c1601650d17
zak000 <zak.essadeq@gmail.Com>
2015-07-12 15:10:29 +0200

    voter.index

--- participer.php
+++ participer.php
@@ -39,40 +39,12 @@ require_once 'fbconfig-participer.php';
     
     
     
-   <?php  error_reporting(E_ALL);
-    ini_set("display_errors", 1);
-    session_start();
-    require_once 'facebook-php-sdk-v4-4.0-dev/autoload.php';
-    use Facebook\FacebookSession;
-	use Facebook\FacebookRedirectLoginHelper;
-	use Facebook\FacebookRequest;
-	use Facebook\GraphUser;
-    use Facebook\FacebookPermissions;
-    use Facebook\FacebookPermissionException;
-    use Facebook\FacebookRequestException;
-    const APPID ="767304380051847";
-    const APPSECRET ="7f0e4cac931818f7f7dc86d722dd5e0e";
-    //$fbPermissions = 'publish_stream,user_photos';  //Required facebook permissions
-    FacebookSession::setDefaultApplication(APPID, APPSECRET);
-    $helper = new FacebookRedirectLoginHelper('https://thewave.herokuapp.com/home.php');
-	//SI les variables de sessions existent et que $_SESSION['fb_token'] existe
-	// alors je veux créer mon utilisateur à partir de cette session
-	if( isset($_SESSION) && isset($_SESSION['fb_token']) )
-	{
-		$session = new FacebookSession($_SESSION['fb_token']);
-	}
-	//Sinon j'affiche le lien de connection
-	else
-	{
-		$session = $helper->getSessionFromRedirect();
-	}
-             
-             
+   <?php 
              
              
              if($session)
 			{
-                 
+                 echo "there is a session";
              }
              else {echo "nooo";
                   }
