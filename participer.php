e86d974b865ae27db07c07669a1c77a35e8839ee
zak000 <zak.essadeq@gmail.Com>
2015-07-12 03:39:14 +0200

    voter.index

--- participer.php
+++ participer.php
@@ -34,10 +34,7 @@ require_once 'fbconfig-participer.php';
     
     
 <form action="upload.php" enctype="multipart/form-data" method="post">
-    <p>
-        Type some text (if you like):<br>
-        <input type="text" name="textline" size="30">
-    </p>
+   
     <p>
         Please specify a file, or a set of files:<br>
         <input type="file" name="datafile" size="40">
@@ -50,10 +47,7 @@ require_once 'fbconfig-participer.php';
     
     
     
-<form action="upload.php" method="post">
-Entrez votre prénom : <input type="text" name="prenom" />
-<input type="submit" value="valider" />
-</form> 
+
     
     
     
