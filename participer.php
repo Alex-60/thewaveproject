236a01a6cdb8a2c2392ab7bbfcb8433e699f75c9
zak000 <zak.essadeq@gmail.Com>
2015-07-12 03:30:19 +0200

    voter.index

--- participer.php
+++ participer.php
@@ -33,7 +33,21 @@ require_once 'fbconfig-participer.php';
     <!-------------------------------------------------------------------------------------------------------------------------------------------->
     
     
-
+<form action="upload.php" enctype="multipart/form-data" method="post">
+    <p>
+        Type some text (if you like):<br>
+        <input type="text" name="textline" size="30">
+    </p>
+    <p>
+        Please specify a file, or a set of files:<br>
+        <input type="file" name="datafile" size="40">
+    </p>
+    <div>
+    <input type="submit" value="Send">
+    </div>
+</form>
+    
+    
     
     
 <form action="upload.php" method="post">
