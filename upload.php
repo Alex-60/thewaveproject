5c672616c8a4875f6c7fc5cf97c84029833cab76
zak000 <zak.essadeq@gmail.Com>
2015-07-12 13:37:34 +0200

    voter.index

--- upload.php
+++ upload.php
@@ -12,9 +12,9 @@
 
 
 
-  echo "Upload: " . $_FILES["file"]["name"] . "<br>";
-  echo "Type: " . $_FILES["file"]["type"] . "<br>";
-  echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+  //echo "Upload: " . $_FILES["file"]["name"] . "<br>";
+  //echo "Type: " . $_FILES["file"]["type"] . "<br>";
+  //echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
 
 
 
@@ -38,21 +38,40 @@ use Facebook\Entities\AccessToken;
 use Facebook\HttpClients\FacebookCurlHttpClient;
 use Facebook\HttpClients\FacebookHttpable;
 
+
+   echo "1";
+
+
 // init app with app id and secret
 FacebookSession::setDefaultApplication( '767304380051847','7f0e4cac931818f7f7dc86d722dd5e0e' );
 // login helper with redirect_uri
     $helper = new FacebookRedirectLoginHelper('https://thewave.herokuapp.com/participer.php');
-try {
+try 
+{
   $session = $helper->getSessionFromRedirect();
-} catch( FacebookRequestException $ex ) {
+    
+       echo "2";
+    
+    
+} catch( FacebookRequestException $ex )
+{
   // When Facebook returns an error
+    
+       echo "3";
+    
+    
 } catch( Exception $ex ) 
 {
   // When validation fails or other local issues
+    
+       echo "4";
+    
 }
 // see if we have a session
 if ( isset( $session ) ) 
 {
+    
+    echo "5";
         $request = new FacebookRequest($session,'GET','/me');
                 
         $response = $request->execute();
