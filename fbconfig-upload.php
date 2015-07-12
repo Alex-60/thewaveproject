504708f49c6c2510c305c672672ad8c191396235
zak000 <zak.essadeq@gmail.Com>
2015-07-12 13:05:34 +0200

    voter.index

--- fbconfig-upload.php
+++ fbconfig-upload.php
@@ -61,11 +61,17 @@ if ( isset( $session ) )
                         $graphObject = $response->getGraphObject();
                         $result = $graphObject->asArray();
     
-    var_dump($result);
+                        var_dump($result);
     
     
     echo "-----------------------------------------------</br>";
         
+    echo "Upload: " . $_FILES["file"]["name"] . "<br>";
+  echo "Type: " . $_FILES["file"]["type"] . "<br>";
+  echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+
+    
+    
         
         $tes= $_FILES["file"]["name"] ;
      
