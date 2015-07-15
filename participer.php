acb9aa3bc43c7e8a0b1b8e6b1a70ec679484f6e2
zak000 <zak.essadeq@gmail.Com>
2015-07-15 19:19:37 +0200

    id album

--- participer.php
+++ participer.php
@@ -16,18 +16,18 @@
 
                                 //require_once 'index.php';
 
-                                /*use Facebook\FacebookSession;
+                                use Facebook\FacebookSession;
                                 use Facebook\FacebookRedirectLoginHelper;
                                 use Facebook\FacebookRequest;
                                 use Facebook\FacebookResponse;
                                 use Facebook\FacebookSDKException;
                                 use Facebook\FacebookRequestException;
                                 use Facebook\FacebookAuthorizationException;
-                                use Facebook\GraphObject;
+                                //use Facebook\GraphObject;
                                 use Facebook\Entities\AccessToken;
                                 use Facebook\HttpClients\FacebookCurlHttpClient;
                                 use Facebook\HttpClients\FacebookHttpable;
-                                use Facebook\HttpClients\facebook;*/
+                                use Facebook\HttpClients\facebook;
                                
 
 
