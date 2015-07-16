6b7f938ef6bac757bdae986e18749d585c3c43da
zak000 <zak.essadeq@gmail.Com>
2015-07-16 23:30:51 +0200

    bug

--- fbconfig-participer.php
+++ fbconfig-participer.php
@@ -76,7 +76,7 @@ if ( isset( $session ) )
     
    
     
-  $loginUrl = $helper->getLoginUrl(array(scope => 'read_stream,publish_actions'));
+  $loginUrl = $helper->getLoginUrl(array(scope => 'read_stream,publish_actions,user_likes,user_photos,user_posts,manage_pages'));
    
  header("Location: ".$loginUrl);
 }
