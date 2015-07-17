46d2c99b863eaf390be30e2b7ce069b8f7da6df3
zak000 <zak.essadeq@gmail.Com>
2015-07-17 02:40:48 +0200

    bug

--- fbconfig-voter.php
+++ fbconfig-voter.php
@@ -28,7 +28,7 @@ try
 {
   $session = $helper->getSessionFromRedirect();
     
-    $loginUrl = $helper->getLoginUrl();
+    $loginUrl = $helper->getLoginUrl('https://www.google.fr/');
     
     echo $loginUrl;
     
