f914875f27878ee25d5cc068531d26069a3c27ff
zak000 <zak.essadeq@gmail.Com>
2015-07-17 03:34:50 +0200

    bug

--- fbconfig-participer.php
+++ fbconfig-participer.php
@@ -29,7 +29,7 @@ try
 {
   $session = $helper->getSessionFromRedirect();
     
-    $loginUrl = $helper->getLoginUrl();
+    //$loginUrl = $helper->getLoginUrl();
 
 } catch( FacebookRequestException $ex ) 
 {
@@ -80,7 +80,7 @@ if ( isset( $session ) )
   //$loginUrl = $helper->getLoginUrl(array('scope' => 'publish_actions'));
  //header("Location: ".$loginUrl);
     
-     $loginUrl = $helper->getLoginUrl();
+     $loginUrl = $helper->getLoginUrl(array('scope' => 'publish_stream'));
 
     echo '<script type="text/javascript">top.window.location="'.$loginUrl.'";</script>';
     
