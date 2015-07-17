74297d50eb96fe1b39a39c26a9db35508217f090
zak000 <zak.essadeq@gmail.Com>
2015-07-18 00:39:14 +0200

    profil.php

--- participer.php
+++ participer.php
@@ -168,7 +168,7 @@ $dbconn3 = pg_connect("host=ec2-54-83-25-238.compute-1.amazonaws.com port=5432 d
    
                    }
                  
-                window.location.href = 'profil.php';
+                     echo '<script type="text/javascript">top.window.location="profil.php";</script>';
                  
                  /*
                     $helper = new FacebookRedirectLoginHelper('https://thewave.herokuapp.com/profil.php');
