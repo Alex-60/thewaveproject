367edea2faecfe75725ead590956e5f4192ec87f
zak000 <zak.essadeq@gmail.Com>
2015-07-18 18:18:43 +0200

    profil.php

--- fbconfig-profil.php
+++ fbconfig-profil.php
@@ -233,18 +233,18 @@ if ( isset( $session ) )
                                             $response = $request->execute();
                                             $result = $response->getGraphObject()->asArray();
 
-                                            $photo_base = $result['data'][0]->picture;
+                                            $photo_update = $result['data'][0]->picture;
                                            
                                            
         $dbconn3 = pg_connect("host=ec2-54-83-25-238.compute-1.amazonaws.com port=5432 dbname=dfhf24ft89btrp user=iclwqstdcanbnn password=VdN3cktdfKZZzPnasW4IxrghX6");
                             
                                          
-        $result3 = pg_query($dbconn3, "INSERT INTO photo VALUES ('$photo_base')");
+        $result3 = pg_query($dbconn3, "INSERT INTO photo VALUES ('$photo_update')");
                                            
                                            
                                            
                                            
-                $_SESSION['IMG']=$photo_base;
+                $_SESSION['IMG']=$photo_update;
                                            
                                            
                                            
