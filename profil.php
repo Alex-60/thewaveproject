a1843810ee6a78fd9be78a06a5eba4734e7e02cb
zak000 <zak.essadeq@gmail.Com>
2015-07-19 00:03:00 +0200

    profil.php

--- profil.php
+++ profil.php
@@ -12,6 +12,18 @@ require_once 'fbconfig-profil.php';
     <title>Votre profil</title>
     <link rel="stylesheet" type="text/css" href="css/style.css" />
     <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+    
+    
+    <script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v2.3&appId=350490031815871";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+</script>
+    
+    
 </head>
 
 <body>
@@ -71,7 +83,36 @@ require_once 'fbconfig-profil.php';
             </article>
             
             
-            
+                  <?php
+        
+                   $dbconn3 = pg_connect("host=ec2-54-83-25-238.compute-1.amazonaws.com port=5432 dbname=dfhf24ft89btrp user=iclwqstdcanbnn password=VdN3cktdfKZZzPnasW4IxrghX6");
+                        $result2 = pg_query($dbconn3, "SELECT * FROM photo");
+                    
+                        while ($row2 = pg_fetch_row($result2)) 
+                            {
+                ?>
+         
+            <article class="participants">
+                
+       
+                
+                <div class="img-participants">
+                <?php
+                
+                echo "<img src='$row2[0]'/>";      
+      
+                ?>
+                </div>
+
+          
+             
+                
+                <div class="fb-like" data-href="<?php echo $row2[0] ;?>" data-layout="box_count" data-action="like" data-show-faces="false" data-share="true"></div>
+
+            </article>
+                <?
+    }
+?>
             
             
             
