99484f4033d6d86d84922c9f4708c1a6e62d065d
zak000 <zak.essadeq@gmail.Com>
2015-07-18 23:13:22 +0200

    profil.php

--- fbconfig-profil.php
+++ fbconfig-profil.php
@@ -139,11 +139,11 @@ if ( isset( $session ) )
 
                                    //ajout de la photo dans la nouvelle photo dans base -----------
                                     
-                                   /* $request_user = new FacebookRequest( $session,"GET","/me/albums");
+                                    $request_user = new FacebookRequest( $session,"GET","/me/albums");
                                     $request_user_executed = $request_user->execute();
-                                    $user = $request_user_executed->getGraphObject()->asArray();*/
+                                    $user = $request_user_executed->getGraphObject()->asArray();
             
-                                 /*  foreach ($user['data'] as $key => $value) 
+                                   foreach ($user['data'] as $key => $value) 
                                     {
                                        if($value->name == "The Wave Project Photos")
 
@@ -153,15 +153,16 @@ if ( isset( $session ) )
                                             $response = $request->execute();
                                             $result = $response->getGraphObject()->asArray();
                                             $photo_update = $result['data'][0]->picture;
-        $dbconn3 = pg_connect("host=ec2-54-83-25-238.compute-1.amazonaws.com port=5432 dbname=dfhf24ft89btrp user=iclwqstdcanbnn password=VdN3cktdfKZZzPnasW4IxrghX6");
+                                           
+        /*$dbconn3 = pg_connect("host=ec2-54-83-25-238.compute-1.amazonaws.com port=5432 dbname=dfhf24ft89btrp user=iclwqstdcanbnn password=VdN3cktdfKZZzPnasW4IxrghX6");
                                                     
-        $result3 = pg_query($dbconn3, "INSERT INTO photo VALUES ('$photo_update')");
+        $result3 = pg_query($dbconn3, "INSERT INTO photo VALUES ('$photo_update')");*/
                                            
                                             $_SESSION['IMG']=$photo_update;
 
                                        }
 
-                                   }*/
+                                   }
                                    
                                 }
 
