f51409bad8b735230394fbd604828efe3aea9060
zak000 <zak.essadeq@gmail.Com>
2015-07-18 03:44:51 +0200

    profil.php

--- profil.php
+++ profil.php
@@ -52,7 +52,7 @@ require_once 'fbconfig-profil.php';
                         <!-- MAX_FILE_SIZE must precede the file input field -->
                         <input type="hidden" name="MAX_FILE_SIZE" value="1048576" />
                         <!-- Name of input element determines name in $_FILES array -->
-                        Send this file: <input name="userfile" type="file" />`
+                        <input name="userfile" type="file" />`
 
                         <input type="submit" name="send" value="Send File" />
                     </form>
