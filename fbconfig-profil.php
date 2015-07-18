c72a3a561cbe0a9b1c4a89e39ec10226f114c37a
zak000 <zak.essadeq@gmail.Com>
2015-07-18 17:34:03 +0200

    profil.php

--- fbconfig-profil.php
+++ fbconfig-profil.php
@@ -45,7 +45,7 @@ try
                  if (isset($_POST['imgchange'])) 
                             {
                                 
-                                    move_uploaded_file($_FILES['file']['tmp_name'], 'upload/' . $_FILES['file']['name']);
+                                    move_uploaded_file($_FILES['file']['tmp_name'], 'tmp/' . $_FILES['file']['name']);
            
                                     die();
 
