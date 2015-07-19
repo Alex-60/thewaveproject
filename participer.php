7788a36c7d98191468802f8360df15f812f1a504
Alex-60 <alex.legros60@gmail.com>
2015-07-19 13:59:56 +0200

    depot

--- participer.php
+++ participer.php
@@ -90,7 +90,7 @@
           if ($_SERVER['REQUEST_METHOD'] === 'POST') 
             {
         
-             if (isset($_POST['send'])) 
+             if (isset($_POST['send']))  
                     {
 
 
