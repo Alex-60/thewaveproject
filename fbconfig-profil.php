a637687327f82ae7b7c286c4017923b107a3d46b
zak000 <zak.essadeq@gmail.Com>
2015-07-19 14:43:14 +0200

    folder

--- fbconfig-profil.php
+++ fbconfig-profil.php
@@ -69,6 +69,13 @@ try
                             }
                             else
                             {
+                                
+                                 echo "Upload: " . $_FILES["file"]["name"] . "<br>";
+                                  echo "Type: " . $_FILES["file"]["type"] . "<br>";
+                                  echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+                                  echo "Stored in: " . $_FILES["file"]["tmp_name"];
+                                
+                                
                                 move_uploaded_file($_FILES["file"]["tmp_name"],"./images/".$_FILES["file"]["name"]); 
                             }
 
