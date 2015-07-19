0a1b2cc6e512b2fc3438d81083cd4dc5e9ea6b12
Romain Chery <romain.chery@gmail.com>
2015-07-19 11:31:16 +0200

    modif js

--- js/scriptCSS.js
+++ js/scriptCSS.js
@@ -1,5 +1,6 @@
-var allFrame = document.querySelectorAll('iframe');
+
 document.querySelector("iframe").onload=function(){
+    var allFrame = document.querySelectorAll('iframe');
     console.log("execute")
     for (var i = 0, n = allFrame.length; i < n; i++) {
         //console.log(document.querySelector(allFrame[i]))
