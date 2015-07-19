1fed1e2ea5e9ac8abc079afbdce38e1e2722c391
Romain Chery <romain.chery@gmail.com>
2015-07-19 11:35:53 +0200

    test js

--- js/scriptCSS.js
+++ js/scriptCSS.js
@@ -1,9 +1,15 @@
-
-document.querySelector("iframe").onload=function(){
-    var allFrame = document.querySelectorAll('iframe');
-    console.log("execute")
-    for (var i = 0, n = allFrame.length; i < n; i++) {
-        //console.log(document.querySelector(allFrame[i]))
-        allFrame[i].style.width = "150px";
-    }
+document.onload=function(){
+    console.log("document charge")
 }
+window.onload=function(){
+    console.log("window charge")
+}
+
+//document.querySelector("iframe").onload=function(){
+//    var allFrame = document.querySelectorAll('iframe');
+//    console.log("execute")
+//    for (var i = 0, n = allFrame.length; i < n; i++) {
+//        //console.log(document.querySelector(allFrame[i]))
+//        allFrame[i].style.width = "150px";
+//    }
+//}
