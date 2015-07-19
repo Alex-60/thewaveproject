78b0fc3aef08005471fc50fd575df6e1c98d9fc9
Romain Chery <romain.chery@gmail.com>
2015-07-19 11:20:09 +0200

    modif js

--- js/scriptCSS.js
+++ js/scriptCSS.js
@@ -1,5 +1,8 @@
-document.onload=function(){
-
-    alert("YOH")
-
+var allFrame = document.querySelectorAll('iframe');
+document.querySelector("iframe").onload=function(){
+    console.log("execute")
+    for (var i = 0, n = allFrame.length; i < n; i++) {
+        //console.log(document.querySelector(allFrame[i]))
+        allFrame[i].style.width = "150px";
+    }
 }
