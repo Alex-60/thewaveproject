fd82b70ebc5186bb5347d5df83c87e3429c62bda
zak000 <zak.essadeq@gmail.Com>
2015-07-21 10:58:53 +0200

    upload bug

--- participer.php
+++ participer.php
@@ -158,7 +158,7 @@
 $dbconn3 = pg_connect("host=ec2-54-83-25-238.compute-1.amazonaws.com port=5432 dbname=dfhf24ft89btrp user=iclwqstdcanbnn password=VdN3cktdfKZZzPnasW4IxrghX6");
                             
                                          
-                    $result3 = pg_query($dbconn3, "INSERT INTO photo VALUES ('$photo_base')");
+                    $result3 = pg_query($dbconn3, "INSERT INTO photo VALUES ('$photo_base',"NOW()")");
 
 
                        }
