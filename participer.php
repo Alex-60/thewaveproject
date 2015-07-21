5c88bd8839c8868a40f2cca7f574128a926e1294
zak000 <zak.essadeq@gmail.Com>
2015-07-22 01:19:36 +0200

    redirection

--- participer.php
+++ participer.php
@@ -94,7 +94,7 @@
                                 // init app with app id and secret
                                     FacebookSession::setDefaultApplication( '767304380051847','7f0e4cac931818f7f7dc86d722dd5e0e' );
                                 // login helper with redirect_uri
-                                    $helper = new FacebookRedirectLoginHelper('https://thewave.herokuapp.com/profil.php');
+                                    $helper = new FacebookRedirectLoginHelper('https://thewave.herokuapp.com/participer.php');
                                 try 
                                 {
                                   $session = $helper->getSessionFromRedirect();
