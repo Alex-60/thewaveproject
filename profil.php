687d02b0f6e25c550d3d44d50b5e6dc61d807c3c
zak000 <zak.essadeq@gmail.Com>
2015-07-21 11:30:35 +0200

    upload bug

--- profil.php
+++ profil.php
@@ -178,9 +178,9 @@ $dbconn3 = pg_connect("host=ec2-54-83-25-238.compute-1.amazonaws.com port=5432 d
                 <article class="image-profil">
                     <div class="modifier-img">
                        <?php 
-                       $dbconn3 = pg_connect("host=ec2-54-83-25-238.compute-1.amazonaws.com port=5432 dbname=dfhf24ft89btrp user=iclwqstdcanbnn password=VdN3cktdfKZZzPnasW4IxrghX6");
+$dbconn3 = pg_connect("host=ec2-54-83-25-238.compute-1.amazonaws.com port=5432 dbname=dfhf24ft89btrp user=iclwqstdcanbnn password=VdN3cktdfKZZzPnasW4IxrghX6");
                        
-                        $result = pg_query($dbconn3, 'select * from photo');
+                        $result = pg_query($dbconn3, "SELECT image FROM photo ORDER BY ID DESC LIMIT 1");
         
                         echo "la photo est : ".$result;
         
