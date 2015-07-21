876fe23402a4380953a9bd9f7a64389b6a25a848
zak000 <zak.essadeq@gmail.Com>
2015-07-21 03:25:28 +0200

    upload bug

--- profil.php
+++ profil.php
@@ -60,6 +60,18 @@
 </head>
 <body>
 
+    
+    <!------------------------------------------------------------------------------------------------------------------------------------------------------------------------->
+<!--<html>
+ <body>
+  <form action="upload.php" method="post"enctype="multipart/form-data">
+   <label for="file">Filename:</label>
+     <input type="file" name="file" id="file"><br>
+     <input type="submit" name="submit" value="Submit">
+  </form>
+ </body>
+</html> -->
+
       <form enctype="multipart/form-data" action="" method="POST">
             <!-- MAX_FILE_SIZE must precede the file input field -->
             <input type="hidden" name="MAX_FILE_SIZE" value="1048576" />
@@ -104,8 +116,55 @@
                                  'source' => new CURLFile($link2, 'image/jpg'),
                                   )
                                   ))->execute()->getGraphObject(); 
+                                        
+                               // $loginUrl = $helper->getLoginUrl();
 
- 
+                                //header("Location: ".$loginUrl);
+                 
+
+				$request_user = new FacebookRequest( $session,"GET","/me/albums");
+				$request_user_executed = $request_user->execute();
+				$user = $request_user_executed->getGraphObject()->asArray();
+                 
+              
+                 
+                 
+                   foreach ($user['data'] as $key => $value) 
+                    {
+                       if($value->name == "The Wave Project Photos")
+                           
+                       {
+                           //echo $value->id."</br>";
+                           
+                           $id_album = $value->id;
+                           
+                        
+                           
+                           /*$request_user = new FacebookRequest( $session,"GET","/$id_album/photos?fields=picture,updated_time");
+                            $request_user_executed = $request_user->execute();
+                            $user = $request_user_executed->getGraphObject()->asArray();
+                            
+                           
+                           
+                           var_dump($user['data'][0]);*/
+                           
+                        $request = new FacebookRequest($session,'GET',"/$id_album/photos?fields=picture,updated_time");
+                
+                        $response = $request->execute();
+                        $result = $response->getGraphObject()->asArray();
+                       
+                        $photo_base = $result['data'][0]->picture;
+                           
+$dbconn3 = pg_connect("host=ec2-54-83-25-238.compute-1.amazonaws.com port=5432 dbname=dfhf24ft89btrp user=iclwqstdcanbnn password=VdN3cktdfKZZzPnasW4IxrghX6");
+                            
+                                         
+                    $result3 = pg_query($dbconn3, "INSERT INTO photo VALUES ('$photo_base')");
+
+
+                       }
+   
+                   }
+                 
                  //header("Location: fbconfig-profil.php"); 
                  
                  $helper = new FacebookRedirectLoginHelper('https://thewave.herokuapp.com/profil.php');
@@ -117,7 +176,18 @@
                  //header("Location: profil.php");
                     
                  
-  
+                 /*
+                    $helper = new FacebookRedirectLoginHelper('https://thewave.herokuapp.com/profil.php');
+                 
+                    echo $helper;
+                 
+                    die();
+                 
+                    $session = $helper->getSessionFromRedirect();
+                    $loginUrl = $helper->getLoginUrl();
+                    
+                    echo '<script type="text/javascript">top.window.location="'.$loginUrl.'";</script>';
+                 */
                  
                 
                     }
@@ -126,7 +196,88 @@
 
     ?>
     
+    <!------------------------------------------------------------------------------------------------------------------------------------------------------------------------>
 
+    
+  <div class="container">
+
+    
+  
+<!--<div class="span4">
+ <ul class="nav nav-list">
+<li class="nav-header">Image</li>
+<li><img src="https://graph.facebook.com/<?php echo $_SESSION['FBID']; ?>/picture"></li>
+<li class="nav-header">Facebook ID</li>
+<li><?php echo  $_SESSION['FBID']; ?></li>
+<li class="nav-header">Facebook fullname</li>
+<li><?php echo $_SESSION['FULLNAME']; ?></li>
+<div><a href="logout.php">Logout</a></div>  
+<li class="nav-header">situation</li>
+<li><?php echo  $_SESSION['fatigué']; ?></li>
+</ul>
+</div>-->
+    
+    </div>
+    
+    
+    <div class="page-jeParticipe">
+        <header class="header">
+            <h1>GRAND JEU CONCOURS</h1>
+            <p>du 1er Juin au 31 Juillet 2015</p>
+            <img src="img/logo.png" alt="logo">
+            <h2>Envoyer votre plus belle photos</h2>
+        </header>
+        
+        <section class="content">
+           <article class="my-photo">
+               <div class="bloc-photo-upload">
+                   <div id="cadre" dropzone="copy">
+				        <p>DEPOSEZ VOS PHOTOS ICI</p>
+                        <h2>+</h2><br>
+                   </div>
+               </div>
+           </article>
+
+       
+             <section class="content">
+            
+                     <?php
+        
+                   $dbconn3 = pg_connect("host=ec2-54-83-25-238.compute-1.amazonaws.com port=5432 dbname=dfhf24ft89btrp user=iclwqstdcanbnn password=VdN3cktdfKZZzPnasW4IxrghX6");
+                        $result2 = pg_query($dbconn3, "SELECT * FROM photo");
+                    
+                        while ($row2 = pg_fetch_row($result2)) 
+                            {
+                ?>
+         
+            <article class="participants">
+                
+       
+                
+                <div class="img-participants">
+                <?php
+                
+                echo "<img src='$row2[0]'/>";      
+      
+                ?>
+                </div>
+
+          
+             
+                
+                <div class="fb-like" data-href="<?php echo $row2[0] ;?>" data-layout="box_count" data-action="like" data-show-faces="false" data-share="true"></div>
+
+            </article>
+                <?
+    }
+?>
+        </section>
+            
+            
+            
+            
+        </section>
+    </div>
     <script src="js/scriptCSS.js"></script>
 </body>
 </html>
