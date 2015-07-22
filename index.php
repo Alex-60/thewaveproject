72b6e9f699e841f5d48eb113b9e12a3e8b5c4bca
zak000 <zak.essadeq@gmail.Com>
2015-07-22 22:33:17 +0200

    index

--- index.php
+++ index.php
@@ -80,11 +80,8 @@ try
             <a href='<?php echo $loginUrl2 ?>' class="btnParticiper">PARTICIPER</a>
                
                <a href='<?php echo '<script type="text/javascript">top.window.location="'.$loginUrl.'";</script>'; ?>'>PARTICIPER</a>
-               
-               
-              
-               
-               <p><?php echo $loginUrl ?></p>
+
+             
                
           
             
