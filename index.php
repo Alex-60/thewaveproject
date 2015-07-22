5adf0a0c8eaded76f7a1bce1ce3ac4144d71ec97
zak000 <zak.essadeq@gmail.Com>
2015-07-22 23:01:57 +0200

    index

--- index.php
+++ index.php
@@ -72,7 +72,7 @@ try
         <article class="slogan">
             <p>JETEZ-VOUS À L'EAU</p>
             <div class="btn-jeux">
-            <a href="<?php echo $loginUrl ?>" class="btnVoter" target="_parent">VOTER</a>
+            <a href="<?php echo $loginUrl ?>" class="btnVoter" target="_top">VOTER</a>
                 
             </div>
            <div class="btn-jeux">
