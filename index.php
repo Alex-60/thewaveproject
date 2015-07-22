ebc8230c92e9bdfb340b2978821dd71764c26d0e
zak000 <zak.essadeq@gmail.Com>
2015-07-22 18:14:45 +0200

    permissions

--- index.php
+++ index.php
@@ -79,7 +79,7 @@ try
                
             <a href='<?php echo $loginUrl2 ?>' class="btnParticiper">PARTICIPER</a>
                
-             <a href="<?php echo $loginUrl2 ;?>" class="btnParticiper">PARTICIPER</a>
+             <a href="<?php echo $loginUrl = $helper->getLoginUrl() ;?>" class="btnParticiper">PARTICIPER</a>
                
  
             </div>
