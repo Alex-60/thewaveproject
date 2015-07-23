62be9aff0f5adf6a50d8761abbbbf4fd912210b2
Alex-60 <alex.legros60@gmail.com>
2015-07-23 09:13:31 +0200

    MAJ include

---
+++ fbconfig.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+// added in v4.0.0
+
+require_once 'autoload.php';
+
+
+//require_once 'index.php';
+  
+use Facebook\FacebookSession;
+use Facebook\FacebookRedirectLoginHelper;
+use Facebook\FacebookRequest;
+use Facebook\FacebookResponse;
+use Facebook\FacebookSDKException;
+use Facebook\FacebookRequestException;
+use Facebook\FacebookAuthorizationException;
+use Facebook\GraphObject;
+use Facebook\Entities\AccessToken;
+use Facebook\HttpClients\FacebookCurlHttpClient;
+use Facebook\HttpClients\FacebookHttpable;
+?>
