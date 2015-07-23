fcaa8b313d211c16c7df9674b17ae1f987c7d9e4
Alex-60 <alex.legros60@gmail.com>
2015-07-23 15:24:10 +0200

    Indentation/Sup

--- voter.php
+++ voter.php
@@ -1,32 +1,28 @@
 <?php
         
+session_start();
 
-       
-                                session_start();
-                                // added in v4.0.0
-                      
-                                require_once 'autoload.php';
-                                //require_once 'index.php';
+require_once 'autoload.php';
+                                
 
-                                use Facebook\FacebookSession;
-                                use Facebook\FacebookRedirectLoginHelper;
-                                use Facebook\FacebookRequest;
-                                use Facebook\FacebookResponse;
-                                use Facebook\FacebookSDKException;
-                                use Facebook\FacebookRequestException;
-                                use Facebook\FacebookAuthorizationException;
-                                use Facebook\GraphObject;
-                                use Facebook\Entities\AccessToken;
-                                use Facebook\HttpClients\FacebookCurlHttpClient;
-                                use Facebook\HttpClients\FacebookHttpable;
-                                use Facebook\HttpClients\facebook;
+use Facebook\FacebookSession;
+use Facebook\FacebookRedirectLoginHelper;
+use Facebook\FacebookRequest;
+use Facebook\FacebookResponse;
+use Facebook\FacebookSDKException;
+use Facebook\FacebookRequestException;
+use Facebook\FacebookAuthorizationException;
+use Facebook\GraphObject;
+use Facebook\Entities\AccessToken;
+use Facebook\HttpClients\FacebookCurlHttpClient;
+use Facebook\HttpClients\FacebookHttpable;
+use Facebook\HttpClients\facebook;
                                
-
-
-        // init app with app id and secret
-            FacebookSession::setDefaultApplication( '767304380051847','7f0e4cac931818f7f7dc86d722dd5e0e' );
-        // login helper with redirect_uri
-            $helper = new FacebookRedirectLoginHelper('https://thewave.herokuapp.com/participer.php');
+// init app with app id and secret
+    FacebookSession::setDefaultApplication( '767304380051847','7f0e4cac931818f7f7dc86d722dd5e0e' );
+// login helper with redirect_uri
+    $helper = new FacebookRedirectLoginHelper('https://thewave.herokuapp.com/participer.php');
+    
     try 
         {
             $session = $helper->getSessionFromRedirect();
@@ -41,66 +37,65 @@
         }
 
        if (isset($_GET['error']) && $_GET['error'] == 'access_denied')
-        {
-           //header('Location: https://thewave.herokuapp.com/');
-        }
+            {
+                //header('Location: https://thewave.herokuapp.com/');
+            }
         else
             {    
 ?>   
                
-<!DOCTYPE html>
-<html lang="fr">
-        <head>
-            <meta charset="UTF-8">
-            <title>Voter</title>
-            <link rel="stylesheet" type="text/css" href="css/style.css" />
-            <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+                    <!DOCTYPE html>
+                    <html lang="fr">
+                            <head>
+                                <meta charset="UTF-8">
+                                <title>Voter</title>
+                                <link rel="stylesheet" type="text/css" href="css/style.css" />
+                                <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
 
-            <div id="fb-root"></div>
-        <script>(function(d, s, id) {
-          var js, fjs = d.getElementsByTagName(s)[0];
-          if (d.getElementById(id)) return;
-          js = d.createElement(s); js.id = id;
-          js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v2.3&appId=350490031815871";
-          fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
-        </script>
+                                <div id="fb-root"></div>
+                            <script>(function(d, s, id) {
+                              var js, fjs = d.getElementsByTagName(s)[0];
+                              if (d.getElementById(id)) return;
+                              js = d.createElement(s); js.id = id;
+                              js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v2.3&appId=350490031815871";
+                              fjs.parentNode.insertBefore(js, fjs);
+                            }(document, 'script', 'facebook-jssdk'));
+                            </script>
 
-        </head>
-        <body>
-            <div class="page-jeVote">
-                <header class="header">
-                    <h1>GRAND JEU CONCOURS</h1>
-                    <p>du 1er Juin au 31 Juillet 2015</p>
-                    <img src="img/logo.png" alt="logo">
-                    <a class="btnParticiper2" href="<?php echo $loginUrl; ?>">PARTICIPER</a>
-                    <h2>Voter pour la meilleure photo</h2>
-                </header>
-                <section class="content">
-                    <?php
-                    $dbconn3 = pg_connect("host=ec2-54-83-25-238.compute-1.amazonaws.com port=5432 dbname=dfhf24ft89btrp user=iclwqstdcanbnn        password=VdN3cktdfKZZzPnasW4IxrghX6");
-                    $result2 = pg_query($dbconn3, "SELECT * FROM photo");
-                    while ($row2 = pg_fetch_row($result2)) 
-                    {
-                        ?>
-                    <article class="participants">
-                        <div class="img-participants">
-                        <?php
-                        echo "<img src='$row2[0]'/>";      
-                        ?>
-                        </div>
-                        <div class="btn-jaime fb-like" data-href="<?php echo $row2[0] ;?>" data-layout="button_count" data-action="like" data-show-faces="false" data-share="false">J'aime</div>
-                    </article>
-                        <?
-                    }
-             
-                    ?>
-                </section>
-            </div>
-            <script src="js/scriptCSS.js"></script>
-        </body>
-</html>
+                            </head>
+                            <body>
+                                <div class="page-jeVote">
+                                    <header class="header">
+                                        <h1>GRAND JEU CONCOURS</h1>
+                                        <p>du 1er Juin au 31 Juillet 2015</p>
+                                        <img src="img/logo.png" alt="logo">
+                                        <a class="btnParticiper2" href="<?php echo $loginUrl; ?>">PARTICIPER</a>
+                                        <h2>Voter pour la meilleure photo</h2>
+                                    </header>
+                                    <section class="content">
+                                        <?php
+                                        $dbconn3 = pg_connect("host=ec2-54-83-25-238.compute-1.amazonaws.com port=5432 dbname=dfhf24ft89btrp user=iclwqstdcanbnn        password=VdN3cktdfKZZzPnasW4IxrghX6");
+                                        $result2 = pg_query($dbconn3, "SELECT * FROM photo");
+                                        while ($row2 = pg_fetch_row($result2)) 
+                                        {
+                                            ?>
+                                        <article class="participants">
+                                            <div class="img-participants">
+                                                <?php
+                                                    echo "<img src='$row2[0]'/>";      
+                                                ?>
+                                            </div>
+                                            <div class="btn-jaime fb-like" data-href="<?php echo $row2[0] ;?>" data-layout="button_count" data-action="like" data-show-faces="false" data-share="false">J'aime</div>
+                                        </article>
+                                            <?
+                                        }
 
-<?
+                                        ?>
+                                    </section>
+                                </div>
+                                <script src="js/scriptCSS.js"></script>
+                            </body>
+                        </html>
+                    <?
             }
 ?>
