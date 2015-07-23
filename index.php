9c6916f4747529f3d76d75b9b750cf6e268a7354
Alex-60 <alex.legros60@gmail.com>
2015-07-23 09:48:50 +0200

    Structure

--- index.php
+++ index.php
@@ -5,25 +5,7 @@
 //session_start(); 
 
 
-session_start();
-// added in v4.0.0
-
-require_once 'autoload.php';
-
-
-//require_once 'index.php';
-  
-use Facebook\FacebookSession;
-use Facebook\FacebookRedirectLoginHelper;
-use Facebook\FacebookRequest;
-use Facebook\FacebookResponse;
-use Facebook\FacebookSDKException;
-use Facebook\FacebookRequestException;
-use Facebook\FacebookAuthorizationException;
-use Facebook\GraphObject;
-use Facebook\Entities\AccessToken;
-use Facebook\HttpClients\FacebookCurlHttpClient;
-use Facebook\HttpClients\FacebookHttpable;
+require_once 'fbconfig.php';
 
 // init app with app id and secret
 FacebookSession::setDefaultApplication( '767304380051847','7f0e4cac931818f7f7dc86d722dd5e0e' );
