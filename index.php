20a90eef623902d3111acbe9227719f4bef20e00
Alex-60 <alex.legros60@gmail.com>
2015-07-23 10:12:06 +0200

    Mise au propre du code

--- index.php
+++ index.php
@@ -90,36 +90,5 @@ use Facebook\HttpClients\FacebookHttpable;
     <?php endif ?>
     
         </body>
-    </section>
- <!--   
-<body>
-  <?php if ($_SESSION['FBID']): ?>      
-<div class="container">
-<div class="hero-unit">
-  <h1>Hello <?php echo $_SESSION['FULLNAME']; ?></h1>
-  <p>Welcome to "facebook login" tutorial</p>
-  </div>
-<div class="span4">
- <ul class="nav nav-list">
-<li class="nav-header">Image</li>
-     
-
-	<li><img src="https://graph.facebook.com/<?php echo $_SESSION['FBID']; ?>/picture"></li>
-<li class="nav-header">Facebook ID</li>
-<li><?php echo  $_SESSION['FBID']; ?></li>
-<li class="nav-header">Facebook fullname</li>
-<li><?php echo $_SESSION['FULLNAME']; ?></li>
-<div><a href="logout.php">Logout</a></div>
-</ul></div></div>
-    <?php else: ?>   
-<div class="container">
-<h1>Login with Facebook</h1>
-           Not Connected
-<div>
-      <a href="fbconfig.php">Login with Facebook</a></div>
-</div>
-    <?php endif ?>
-</body>-->
-    
-    
+    </section>    
 </html>
